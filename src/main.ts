a962b0b84cc60c8a2628abdf8517ebf0b09a36c8
Llandy Riveron Del Risco <llandy3d@gmail.com>
2024-05-28 11:16:38 +0200

    bootstrap: base electron application (#1)
    * forge vite-typescript template
    
    * update package.json info
    
    * devtools in detached mode
    
    * prevent white screen showing
    
    * add pr link for whenReady
    
    * Update tsconfig.json
    
    Co-authored-by: Edgar Fisher <fisher.edgar@gmail.com>
    
    ---------
    
    Co-authored-by: Edgar Fisher <fisher.edgar@gmail.com>

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  }
+
+  // wait for the window to be ready before showing it. It prevents showing a white page on longer load times.
+  mainWindow.once('ready-to-show', () => {
+      mainWindow.show();
+      mainWindow.focus();
+  });
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools({ mode: "detach" });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+// https://github.com/electron/electron/pull/21972
+app.whenReady().then(() => {
+  createWindow();
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
