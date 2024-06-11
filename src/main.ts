10b937e22ad9b67bd5e5e306b9e9c182ab52de27
Edgar Fisher <fisher.edgar@gmail.com>
2024-06-11 16:49:48 +0300

    tools: update typescript and fix ts errors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import path from 'path'
 import { launchProxy, type ProxyProcess } from './proxy'
 import { launchBrowser } from './browser'
 import { Process } from '@puppeteer/browsers'
+import { runScript, showScriptSelectDialog, type K6Process } from './script'
 import { writeFile } from 'fs/promises'
 
-let currentProxyProcess: ProxyProcess
-let currentBrowserProcess: Process
+let currentProxyProcess: ProxyProcess | null
+let currentBrowserProcess: Process | null
+let currentk6Process: K6Process | null
 let harBuffer: string
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -73,6 +75,11 @@ app.on('activate', () => {
 ipcMain.on('proxy:start', async (event) => {
   console.info('proxy:start event received')
   const browserWindow = BrowserWindow.fromWebContents(event.sender)
+
+  if (!browserWindow) {
+    throw new Error('failed to obtain browserWindow')
+  }
+
   currentProxyProcess = launchProxy(browserWindow)
 })
 
@@ -89,7 +96,7 @@ ipcMain.on('browser:start', async (event) => {
   console.info('browser:start event received')
   const browserWindow = BrowserWindow.fromWebContents(event.sender)
   currentBrowserProcess = await launchBrowser()
-  browserWindow.webContents.send('browser:started')
+  browserWindow?.webContents.send('browser:started')
   console.info('browser:started event sent')
 })
 
@@ -98,6 +105,39 @@ ipcMain.on('browser:stop', async () => {
   if (currentBrowserProcess) {
     currentBrowserProcess.close()
     currentBrowserProcess = null
+  }
+})
+
+// Script
+ipcMain.handle('script:select', async (event) => {
+  console.info('script:select event received')
+  const browserWindow = BrowserWindow.fromWebContents(event.sender)
+
+  if (!browserWindow) {
+    throw new Error('failed to obtain browserWindow')
+  }
+
+  const scriptPath = await showScriptSelectDialog(browserWindow)
+  console.info(`selected script: ${scriptPath}`)
+  return scriptPath
+})
+
+ipcMain.on('script:run', async (event, scriptPath: string) => {
+  console.info('script:run event received')
+  const browserWindow = BrowserWindow.fromWebContents(event.sender)
+
+  if (!browserWindow) {
+    throw new Error('failed to obtain browserWindow')
+  }
+
+  currentk6Process = await runScript(browserWindow, scriptPath)
+})
+
+ipcMain.on('script:stop', () => {
+  console.info('script:stop event received')
+  if (currentk6Process) {
+    currentk6Process.kill()
+    currentk6Process = null
   }
 })
 
