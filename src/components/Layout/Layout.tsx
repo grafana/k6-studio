a55ff10ce9c0e0a2292324b37bc637045fc776fd
Uladzimir Dzmitračkoŭ <3773351+going-confetti@users.noreply.github.com>
2024-06-25 12:31:04 +0200

    dev: Add react router (#10)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,48 +1,11 @@
-import {
-  Box,
-  Text,
-  Flex,
-  Button,
-  Separator,
-  ScrollArea,
-} from '@radix-ui/themes'
-import { Recorder } from '@/views/Recorder'
-import { useState } from 'react'
-import { Validator } from '@/views/Validator/Validator'
+import { Box, Flex, ScrollArea } from '@radix-ui/themes'
+import { Outlet } from 'react-router-dom'
+import { SidebarNav } from './SidebarNav'
 
 export function Layout() {
-  const [tab, setTab] = useState('recorder')
   return (
     <Flex overflow="hidden" maxWidth="100%">
-      <Flex
-        width="200px"
-        minWidth="200px"
-        align="center"
-        direction="column"
-        style={{ borderRight: '1px solid var(--gray-4)' }}
-      >
-        <Box pt="3" pb="2">
-          <Text size="6" weight="bold" color="violet">
-            k6 studio
-          </Text>
-        </Box>
-        <Separator size="4" style={{ backgroundColor: 'var(--gray-4)' }} />
-
-        <Flex direction="column" width="100%" p="3" gap="1">
-          <Button
-            onClick={() => setTab('recorder')}
-            variant={tab === 'recorder' ? 'solid' : 'soft'}
-          >
-            <Text>Recorder</Text>
-          </Button>
-          <Button
-            onClick={() => setTab('validator')}
-            variant={tab === 'validator' ? 'solid' : 'soft'}
-          >
-            <Text>Validator</Text>
-          </Button>
-        </Flex>
-      </Flex>
+      <SidebarNav />
 
       <Flex flexGrow="1" style={{ backgroundColor: 'var(--gray-3)' }} asChild>
         <ScrollArea style={{ height: '100dvh' }} scrollbars="vertical">
@@ -55,8 +18,7 @@ export function Layout() {
             p="4"
             m="4"
           >
-            {tab === 'recorder' && <Recorder />}
-            {tab === 'validator' && <Validator />}
+            <Outlet />
           </Box>
         </ScrollArea>
       </Flex>
