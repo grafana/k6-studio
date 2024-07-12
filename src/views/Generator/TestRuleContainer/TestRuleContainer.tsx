07dfcec4e7a6a36ee5be0d9c8b37eb2c78c18b52
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-07-12 17:13:58 +0200

    use ScrollArea

--- src/views/Generator/TestRuleContainer/TestRuleContainer.tsx
+++ src/views/Generator/TestRuleContainer/TestRuleContainer.tsx
@@ -1,5 +1,5 @@
 import { useGeneratorStore } from '@/hooks/useGeneratorStore'
-import { Flex } from '@radix-ui/themes'
+import { Flex, ScrollArea } from '@radix-ui/themes'
 import { TestRuleItem } from './TestRule'
 import { NewRuleMenu } from '../NewRuleMenu'
 
@@ -7,33 +7,27 @@ export function TestRuleContainer() {
   const { rules, selectedRuleId } = useGeneratorStore()
 
   return (
-    <Flex
-      direction="column"
-      gap="1"
-      height="100%"
-      p="2"
-      // TODO: check if scrollbars looks the same across platforms and either
-      // remove or replace ScrollArea with native scrollbars
-      style={{ overflowY: 'auto', paddingTop: '0 ' }}
-    >
-      <Flex
-        position="sticky"
-        align="center"
-        justify="between"
-        top="0"
-        py="2"
-        style={{ background: 'var(--color-background)' }}
-      >
-        Test rules ({rules.length})<NewRuleMenu />
-      </Flex>
+    <Flex direction="column" gap="1" height="100%" p="2" pt="0">
+      <ScrollArea scrollbars="vertical">
+        <Flex
+          position="sticky"
+          align="center"
+          justify="between"
+          top="0"
+          py="2"
+          style={{ background: 'var(--color-background)' }}
+        >
+          Test rules ({rules.length})<NewRuleMenu />
+        </Flex>
 
-      {rules.map((rule, i) => (
-        <TestRuleItem
-          rule={rule}
-          isSelected={rule.id === selectedRuleId}
-          key={i}
-        />
-      ))}
+        {rules.map((rule, i) => (
+          <TestRuleItem
+            rule={rule}
+            isSelected={rule.id === selectedRuleId}
+            key={i}
+          />
+        ))}
+      </ScrollArea>
     </Flex>
   )
 }
