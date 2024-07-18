5abcd9fc96235dc45e259af1feec1342b17f0722
Llandy Riveron Del Risco <llandy3d@gmail.com>
2024-07-18 02:33:07 +0200

    refactor TestGeneratorOptions -> TestOptions

--- src/schemas/generator.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-import { ThinkTime, LoadProfileExecutorOptions } from '@/schemas/testOptions'
-
-export const GeneratorOptions = z.object({
-  loadProfile: LoadProfileExecutorOptions,
-  thinkTime: ThinkTime,
-})
-
-export type GeneratorOptions = z.infer<typeof GeneratorOptions>
