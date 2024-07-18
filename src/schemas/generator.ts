79bd8f78f100786e379e3fcd3d8c69c0fa151cce
Llandy Riveron Del Risco <llandy3d@gmail.com>
2024-07-18 02:13:23 +0200

    GeneratorOptions schema

--- src/schemas/generator.ts
+++ src/schemas/generator.ts
@@ -1,4 +1,12 @@
 import { z } from 'zod'
+import { ThinkTime, LoadProfileExecutorOptions } from '@/schemas/testOptions'
+
+export const GeneratorOptions = z.object({
+  loadProfile: LoadProfileExecutorOptions,
+  thinkTime: ThinkTime,
+})
+
+export type GeneratorOptions = z.infer<typeof GeneratorOptions>
 
 const Variable = z.object({
   name: z.string(),
