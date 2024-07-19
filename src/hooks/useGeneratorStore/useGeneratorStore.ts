38218534d2cfed3d71d22122a71b010d03afbb79
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-07-19 17:45:32 +0200

    refactor: setGeneratorFile action

--- src/hooks/useGeneratorStore/useGeneratorStore.ts
+++ src/hooks/useGeneratorStore/useGeneratorStore.ts
@@ -8,6 +8,8 @@ import {
   createTestOptionsSlice,
 } from './slices'
 import { createRecordingSlice } from './slices/recording'
+import { ExecutorType } from '@/constants/generator'
+import { exhaustive } from '@/utils/typescript'
 
 export const useGeneratorStore = create<GeneratorState>()(
   immer((set, ...rest) => ({
@@ -16,9 +18,52 @@ export const useGeneratorStore = create<GeneratorState>()(
     ...createTestDataSlice(set, ...rest),
     ...createTestOptionsSlice(set, ...rest),
     name: generateNewName(),
-    setName: (name: string) =>
+    setName: (name) =>
       set((state) => {
         state.name = name
+      }),
+    setGeneratorFile: (
+      {
+        name,
+        options: { thinkTime, loadProfile },
+        testData: { variables },
+        recordingPath,
+        rules,
+        allowlist,
+      },
+      recording = []
+    ) =>
+      set((state) => {
+        state.name = name
+        // options
+        state.sleepType = thinkTime.sleepType
+        state.timing = thinkTime.timing
+        state.executor = loadProfile.executor
+        state.startTime = loadProfile.startTime
+        state.gracefulStop = loadProfile.gracefulStop
+        switch (loadProfile.executor) {
+          case ExecutorType.RampingVUs:
+            state.stages = loadProfile.stages
+            state.gracefulRampDown = loadProfile.gracefulRampDown
+            state.startVUs = loadProfile.startVUs
+            break
+          case ExecutorType.SharedIterations:
+            state.iterations = loadProfile.iterations
+            state.maxDuration = loadProfile.maxDuration
+            state.vus = loadProfile.vus
+            break
+          default:
+            exhaustive(loadProfile)
+        }
+        // data
+        state.variables = variables
+        // recording
+        state.requests = recording
+        state.recordingPath = recordingPath
+        state.showAllowListDialog = false
+        state.allowList = allowlist
+        // rules
+        state.rules = rules
       }),
   }))
 )
