6ce828d8c4fa70cca475a87adbdb75856849c6f2
Uladzimir Dzmitračkoŭ <3773351+going-confetti@users.noreply.github.com>
2024-07-24 15:00:55 +0200

    fix: Redirect to load profile on rule editor close (#63)

--- src/views/Generator/GeneratorDrawer/RuleEditor/RuleEditor.tsx
+++ src/views/Generator/GeneratorDrawer/RuleEditor/RuleEditor.tsx
@@ -4,6 +4,7 @@ import { CorrelationEditor } from './CorrelationEditor'
 import { CustomCodeEditor } from './CustomCodeEditor'
 import { Box, Callout, IconButton } from '@radix-ui/themes'
 import { Cross2Icon, InfoCircledIcon } from '@radix-ui/react-icons'
+import { useNavigate } from 'react-router-dom'
 
 export function RuleEditorSwitch() {
   const rule = useGeneratorStore(selectSelectedRule)
@@ -34,19 +35,20 @@ export function RuleEditorSwitch() {
 }
 
 export function RuleEditor() {
+  const navigate = useNavigate()
   const selectRule = useGeneratorStore((store) => store.selectRule)
   const selectedRuleId = useGeneratorStore((store) => store.selectedRuleId)
+
+  const handleClose = () => {
+    selectRule(null)
+    navigate('/generator')
+  }
 
   return (
     <Box position="relative">
       {selectedRuleId && (
         <Box position="absolute" right="0" top="0">
-          <IconButton
-            variant="ghost"
-            title="close"
-            m="2"
-            onClick={() => selectRule(null)}
-          >
+          <IconButton variant="ghost" title="close" m="2" onClick={handleClose}>
             <Cross2Icon />
           </IconButton>
         </Box>
