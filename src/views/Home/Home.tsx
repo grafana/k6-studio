eed279125f15e641e99744511544b762002a9546
Uladzimir Dzmitračkoŭ <3773351+going-confetti@users.noreply.github.com>
2024-07-25 10:04:54 +0200

    feat: Use Navigation cards in Home view (#65)

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,40 +1,29 @@
-import { Button, Flex, Text } from '@radix-ui/themes'
-import { useNavigate } from 'react-router-dom'
+import { Flex, Grid, Text } from '@radix-ui/themes'
+import { NavigationCard } from './NavigationCard'
 
 export function Home() {
-  const navigate = useNavigate()
-
   return (
     <Flex direction="column" align="center" justify="center" height="100%">
       <Text size="3" weight="bold" mb="4">
         Welcome to k6 studio 👋
       </Text>
-      <Flex gap="2">
-        <Button
-          variant="solid"
-          onClick={() => {
-            navigate('recorder')
-          }}
-        >
-          Record flow
-        </Button>
-        <Button
-          variant="solid"
-          onClick={() => {
-            navigate('generator')
-          }}
-        >
-          Generate test
-        </Button>
-        <Button
-          variant="solid"
-          onClick={() => {
-            navigate('validator')
-          }}
-        >
-          Validate script
-        </Button>
-      </Flex>
+      <Grid gap="3" columns="3" maxWidth="600px">
+        <NavigationCard
+          title="Record flow"
+          description="Use our built-in proxy to record a user flow"
+          to="recorder"
+        />
+        <NavigationCard
+          title="Generate test"
+          description="Transform a recorded flow into a k6 test script"
+          to="generator"
+        />
+        <NavigationCard
+          title="Validate script"
+          description="Debug and validate your k6 script"
+          to="validator"
+        />
+      </Grid>
     </Flex>
   )
 }
