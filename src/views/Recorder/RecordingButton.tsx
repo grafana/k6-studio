2d3348bf88ef671daf361d84b0abc18822c58ac5
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-07-31 10:44:53 +0200

    Home

--- src/views/Recorder/RecordingButton.tsx
+++ src/views/Recorder/RecordingButton.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Button } from '@radix-ui/themes'
 import { PlayIcon, StopIcon } from '@radix-ui/react-icons'
 
@@ -7,17 +7,14 @@ import { startRecording, stopRecording } from './Recorder.utils'
 import { useRecorderStore } from '@/store/recorder'
 import { proxyDataToHar } from '@/utils/proxyDataToHar'
 import { groupProxyData } from '@/utils/groups'
-import { createNewGeneratorFile } from '@/utils/generator'
 
 export function RecordingControls() {
   const requests = useRecorderStore((state) => state.proxyData)
   const isRecording = useRecorderStore((state) => state.isRecording)
   const setIsRecording = useRecorderStore((state) => state.setIsRecording)
   const resetProxyData = useRecorderStore((state) => state.resetProxyData)
-  const isEmpty = useRecorderStore((state) => state.proxyData.length === 0)
   const navigate = useNavigate()
   const [isLoading, setIsLoading] = useState(false)
-  const { path } = useParams()
 
   async function handleStartRecording() {
     resetProxyData()
@@ -40,47 +37,25 @@ export function RecordingControls() {
         JSON.stringify(har, null, 4)
       )
 
-      navigate(`/recorder/${encodeURIComponent(filePath)}`)
+      navigate(`/recording-previewer/${encodeURIComponent(filePath)}`)
     }
-  }
-
-  async function handleCreateTestGenerator() {
-    if (!path) {
-      return
-    }
-
-    const newGenerator = createNewGeneratorFile(path)
-    const generatorPath = await window.studio.generator.saveGenerator(
-      JSON.stringify(newGenerator, null, 2),
-      `${new Date().toISOString()}.json`
-    )
-
-    navigate(`/generator/${encodeURIComponent(generatorPath)}`)
   }
 
   return (
-    <>
-      <Button
-        onClick={isRecording ? handleStopRecording : handleStartRecording}
-        loading={isLoading}
-        color={isRecording ? 'red' : 'green'}
-      >
-        <Icon recording={isRecording} />
-        {isRecording ? 'Stop recording' : 'Start recording'}
-      </Button>
-      {!isRecording && !isEmpty && (
-        <Button onClick={handleCreateTestGenerator}>
-          Create test generator
-        </Button>
+    <Button
+      onClick={isRecording ? handleStopRecording : handleStartRecording}
+      loading={isLoading}
+      color={isRecording ? 'red' : 'green'}
+    >
+      {isRecording ? (
+        <>
+          <StopIcon /> Stop recording
+        </>
+      ) : (
+        <>
+          <PlayIcon /> Start recording
+        </>
       )}
-    </>
+    </Button>
   )
-}
-
-function Icon({ recording }: { recording: boolean }) {
-  if (recording) {
-    return <StopIcon />
-  }
-
-  return <PlayIcon />
 }
