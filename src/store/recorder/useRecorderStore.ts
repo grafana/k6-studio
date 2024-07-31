1ef3ebca543e6a60a691b4f48ecc7415bec188de
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-07-31 11:21:45 +0200

    remove redundant properties from recorder store

--- src/store/recorder/useRecorderStore.ts
+++ src/store/recorder/useRecorderStore.ts
@@ -5,14 +5,12 @@ import { ProxyData } from '@/types'
 import { mergeRequestsById } from '@/views/Recorder/Recorder.utils'
 
 interface State {
-  path: string | null
   isRecording: boolean
   proxyData: ProxyData[]
 }
 
 interface Actions {
   addRequest: (request: ProxyData, currentGroup: string) => void
-  setPath: (path: string) => void
   setIsRecording: (isRecording: boolean) => void
   setProxyData: (proxyData: ProxyData[]) => void
   resetProxyData: () => void
@@ -33,7 +31,6 @@ export const useRecorderStore = create<RecorderStore>()(
           group: currentGroup,
         })
       }),
-    setPath: (path) => set((state) => (state.path = path)),
     setIsRecording: (isRecording) =>
       set((state) => {
         state.isRecording = isRecording
