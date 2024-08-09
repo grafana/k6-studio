726b2a474bc373bee5508a9041cf4095afe222fd
Edgar Fisher <fisher.edgar@gmail.com>
2024-08-09 13:05:49 +0300

    fix: cleanup

--- src/components/Toast/Toasts.tsx
+++ src/components/Toast/Toasts.tsx
@@ -1,73 +1,13 @@
-import { useToast, useToastStore } from '@/store/ui/useToast'
+import { useToastStore } from '@/store/ui/useToast'
 import * as RadixToast from '@radix-ui/react-toast'
 import { Toast } from './Toast'
 import { ToastViewport } from './Toast.styles'
-import { Button } from '@radix-ui/themes'
 
 export function Toasts() {
   const toasts = useToastStore((state) => state.toasts)
-  const addToast = useToast()
 
   return (
     <RadixToast.Provider swipeDirection="right">
-      <button
-        onClick={() => {
-          addToast({
-            title: 'Hello world!',
-            action: <Button>undo</Button>,
-            status: 'success',
-          })
-        }}
-      >
-        with action
-      </button>
-
-      <button
-        onClick={() => {
-          addToast({
-            title: 'Just title',
-            status: 'error',
-          })
-        }}
-      >
-        just title
-      </button>
-      <button
-        onClick={() => {
-          addToast({
-            title: 'Hello world!',
-            description:
-              'just a simple description that could be a potentially long text',
-          })
-        }}
-      >
-        with description
-      </button>
-
-      <button
-        onClick={() => {
-          addToast({
-            title: 'Hello world!',
-            description:
-              'just a simple description that could be a potentially long text',
-          })
-        }}
-      >
-        with description
-      </button>
-      <button
-        onClick={() => {
-          addToast({
-            title: 'Hello world!',
-            description:
-              'just a simple description that could be a potentially long text',
-            action: <Button>undo</Button>,
-            status: 'success',
-          })
-        }}
-      >
-        with description and action
-      </button>
       {toasts.map((toast) => (
         <Toast toast={toast} key={toast.id} />
       ))}
