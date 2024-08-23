f0f1714478a927e56464a8dd1f40d5ad7ccae197
Cristiano Ventura <mrk.cristiano@gmail.com>
2024-08-23 14:47:06 -0400

    feat: use react-json-view to preview json objects

--- src/components/WebLogView/ResponseDetails/Raw.tsx
+++ src/components/WebLogView/ResponseDetails/Raw.tsx
@@ -1,20 +1,15 @@
 import { Text } from '@radix-ui/themes'
-import { css } from '@emotion/react'
+import { ReadOnlyEditor } from '@/components/Monaco/ReadOnlyEditor'
 
 interface RawProps {
   content: string
+  format: string
 }
 
-export function Raw({ content }: RawProps) {
+export function Raw({ content, format }: RawProps) {
   return (
     <Text size="1" wrap="pretty">
-      <pre css={style}>
-        <code>{content}</code>
-      </pre>
+      <ReadOnlyEditor language={format} value={content} />
     </Text>
   )
 }
-
-const style = css`
-  margin: 0;
-`
