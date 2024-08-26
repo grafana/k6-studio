91af7b18da21e5f94c9ec50cb25d027994230b67
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-08-26 15:51:35 +0200

    chore: remove unused utils

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -10,22 +10,3 @@ export function generateFileNameWithTimestamp(
       .split('.')[0] ?? ''
   return `${prefix ? `${prefix} - ` : ''}${timestamp}.${extension}`
 }
-
-function normalizeFilePath(path: string): string {
-  return path.replace(/\\/g, '/')
-}
-
-export function encodeFilePath(path: string): string {
-  const normalizedPath = normalizeFilePath(path)
-
-  return encodeURIComponent(normalizedPath)
-}
-
-export function decodeFilePath(encodedPath: string): string {
-  const decodedPath = decodeURIComponent(encodedPath)
-  if (process.platform === 'win32') {
-    return decodedPath.replace(/\//g, '\\')
-  }
-
-  return decodedPath
-}
