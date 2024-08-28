54905b761693d34a76a1d00e32f29319671b3325
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-08-28 21:10:13 +0200

    fix: Rule preview has no padding

--- src/views/Generator/RulePreview/CorrelationPreview.tsx
+++ src/views/Generator/RulePreview/CorrelationPreview.tsx
@@ -1,4 +1,4 @@
-import { Box, Callout, Code, Heading, ScrollArea } from '@radix-ui/themes'
+import { Box, Callout, Code, Flex, Heading, ScrollArea } from '@radix-ui/themes'
 import { TestRule, CorrelationRule, CorrelationStateMap } from '@/types/rules'
 import { selectFilteredRequests, useGeneratorStore } from '@/store/generator'
 import { applyRule } from '@/rules/rules'
@@ -8,6 +8,7 @@ import { useMemo, useState } from 'react'
 import { WebLogView } from '@/components/WebLogView'
 import { Allotment } from 'allotment'
 import { Details } from '@/components/WebLogView/Details'
+import { css } from '@emotion/react'
 
 export function CorrelationPreview({ rule }: { rule: CorrelationRule }) {
   const [selectedRequest, setSelectedRequest] = useState<ProxyData | null>(null)
@@ -22,49 +23,60 @@ export function CorrelationPreview({ rule }: { rule: CorrelationRule }) {
   return (
     <Allotment defaultSizes={[1, 2]} vertical>
       <Allotment.Pane minSize={200}>
-        <Heading size="3" mb="3">
-          Preview
-        </Heading>
-        {!result && (
-          <Callout.Root color="amber" role="alert" variant="surface">
-            <Callout.Text wrap="balance">No requests matched</Callout.Text>
-          </Callout.Root>
-        )}
-        <ScrollArea>
-          <Box height="100%">
-            {result && (
-              <>
-                <Heading size="2" mb="2">
-                  Requests matched
-                </Heading>
-                <WebLogView
-                  requests={result.responsesExtracted}
-                  onSelectRequest={setSelectedRequest}
-                />
-                <Heading size="2" my="2">
-                  Extracted value:{' '}
-                </Heading>
-                <pre>
-                  <Code>{JSON.stringify(result.extractedValue, null, 2)}</Code>
-                </pre>
-
-                {result.requestsReplaced.length > 0 && (
-                  <>
-                    <Heading size="2" my="2">
-                      Value replaced in
-                    </Heading>
+        <Flex height="100%">
+          {!result && (
+            <Box p="2">
+              <Callout.Root color="amber" role="alert" variant="surface">
+                <Callout.Text wrap="balance">No requests matched</Callout.Text>
+              </Callout.Root>
+            </Box>
+          )}
+          <ScrollArea scrollbars="vertical">
+            <Box
+              css={css`
+                flex-grow: 1;
+              `}
+            >
+              {result && (
+                <>
+                  <Heading size="2" m="2">
+                    Requests matched
+                  </Heading>
+                  <Box>
                     <WebLogView
-                      requests={requestsReplacedToProxyData(
-                        result.requestsReplaced
-                      )}
-                      onSelectRequest={console.log}
+                      requests={result.responsesExtracted}
+                      onSelectRequest={setSelectedRequest}
                     />
-                  </>
-                )}
-              </>
-            )}
-          </Box>
-        </ScrollArea>
+                  </Box>
+                  <Heading size="2" m="2">
+                    Extracted value:{' '}
+                  </Heading>
+                  <Box px="2">
+                    <pre>
+                      <Code>
+                        {JSON.stringify(result.extractedValue, null, 2)}
+                      </Code>
+                    </pre>
+                  </Box>
+
+                  {result.requestsReplaced.length > 0 && (
+                    <>
+                      <Heading size="2" m="2">
+                        Value replaced in
+                      </Heading>
+                      <WebLogView
+                        requests={requestsReplacedToProxyData(
+                          result.requestsReplaced
+                        )}
+                        onSelectRequest={console.log}
+                      />
+                    </>
+                  )}
+                </>
+              )}
+            </Box>
+          </ScrollArea>
+        </Flex>
       </Allotment.Pane>
       <Allotment.Pane minSize={300} visible={selectedRequest !== null}>
         <Details
