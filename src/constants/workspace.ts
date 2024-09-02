51d346cf293255edb59ed66836f2458484226b3e
Llandy Riveron Del Risco <llandy3d@gmail.com>
2024-09-02 15:23:01 +0200

    set project path to userData

--- src/constants/workspace.ts
+++ src/constants/workspace.ts
@@ -1,7 +1,7 @@
 import { app } from 'electron'
 import path from 'path'
 
-export const PROJECT_PATH = path.join(app.getPath('documents'), 'k6-studio')
+export const PROJECT_PATH = path.join(app.getPath('userData'), 'k6-studio')
 export const RECORDINGS_PATH = path.join(PROJECT_PATH, 'Recordings')
 export const GENERATORS_PATH = path.join(PROJECT_PATH, 'Generators')
 export const SCRIPTS_PATH = path.join(PROJECT_PATH, 'Scripts')
