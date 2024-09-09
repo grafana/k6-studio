778e0bacaebc7d5de5ffae808fc473c5354ee032
Cristiano Ventura <mrk.cristiano@gmail.com>
2024-09-09 10:30:46 -0400

    fix: add subgroups to checks array

--- resources/checks_snippet.js
+++ resources/checks_snippet.js
@@ -1,13 +1,26 @@
 export function handleSummary(data) {
   const checks = []
+
+  function traverseGroup(group) {
+    if (group.checks) {
+      group.checks.forEach((check) => {
+        checks.push(check)
+      })
+    }
+    if (group.groups) {
+      group.groups.forEach((subGroup) => {
+        traverseGroup(subGroup)
+      })
+    }
+  }
+
   data.root_group.checks.forEach((check) => {
     checks.push(check)
   })
   data.root_group.groups.forEach((group) => {
-    group.checks.forEach((check) => {
-      checks.push(check)
-    })
+    traverseGroup(group)
   })
+
   return {
     stdout: JSON.stringify(checks),
   }
