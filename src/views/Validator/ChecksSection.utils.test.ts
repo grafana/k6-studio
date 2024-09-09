93002ec7bf96749719bc0b420bf422fec098d85e
Cristiano Ventura <mrk.cristiano@gmail.com>
2024-09-08 20:21:00 -0400

    test: test for subgroups

--- src/views/Validator/ChecksSection.utils.test.ts
+++ src/views/Validator/ChecksSection.utils.test.ts
@@ -34,13 +34,21 @@ describe('Checks Section - utils', () => {
       const check2 = buildCheck({ path: '::check2' })
       const check3 = buildCheck({ path: '::group1::check3' })
       const check4 = buildCheck({ path: '::group2::check4' })
+      const check5 = buildCheck({ path: '::group2::subgroup::check5' })
 
-      const groupedChecks = groupChecksByPath([check1, check2, check3, check4])
+      const groupedChecks = groupChecksByPath([
+        check1,
+        check2,
+        check3,
+        check4,
+        check5,
+      ])
 
       const expected = {
         default: [check1, check2],
         group1: [check3],
         group2: [check4],
+        'group2::subgroup': [check5],
       }
 
       expect(groupedChecks).toMatchObject(expected)
