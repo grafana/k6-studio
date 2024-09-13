4298bc9087787611b1538f73d4e77a2d02d1fb8c
Edgar Fisher <fisher.edgar@gmail.com>
2024-09-13 15:36:52 +0300

    fix: don't allow duplicate names for variables

--- src/schemas/testData.ts
+++ src/schemas/testData.ts
@@ -6,5 +6,19 @@ export const VariableSchema = z.object({
 })
 
 export const TestDataSchema = z.object({
-  variables: VariableSchema.array(),
+  variables: VariableSchema.array().superRefine((variables, ctx) => {
+    const names = variables.map((variable) => variable.name)
+
+    const duplicateIndex = variables.findIndex(
+      (item, index) => names.indexOf(item.name) !== index
+    )
+
+    if (duplicateIndex !== -1) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Variable names must be unique',
+        path: [duplicateIndex, 'name'],
+      })
+    }
+  }),
 })
