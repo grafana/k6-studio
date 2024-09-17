73f48a73edd40a02aba659bf44e2724580a30bc5
Edgar Fisher <fisher.edgar@gmail.com>
2024-09-17 15:21:54 +0300

    feat: add experimental banner

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Grid, Text } from '@radix-ui/themes'
+import { Button, Flex, Grid, Heading, Text } from '@radix-ui/themes'
 import { NavigationCard } from './NavigationCard'
 import { createNewGeneratorFile } from '@/utils/generator'
 import { useNavigate } from 'react-router-dom'
@@ -10,6 +10,7 @@ import {
   PlusCircledIcon,
 } from '@radix-ui/react-icons'
 import { generateFileNameWithTimestamp } from '@/utils/file'
+import { ExperimentalBanner } from '@/components/ExperimentalBanner'
 
 export function Home() {
   const navigate = useNavigate()
@@ -36,49 +37,55 @@ export function Home() {
   }
 
   return (
-    <Flex direction="column" align="center" justify="center" height="100%">
-      <Text size="8">Discover what you can do with k6 Studio</Text>
-      <Text
-        css={css`
-          color: var(--gray-11);
-          font-size: 14px;
-          line-height: 22px;
-          max-width: 650px;
-          text-align: center;
-          margin-bottom: var(--space-6);
-        `}
-      >
-        Uncover bottlenecks, accelerate load times, and deliver exceptional user
-        experiences with the industry{"'"}s leading performance testing tool.
-      </Text>
-      <Grid gap="8" columns="3" maxWidth="720px">
-        <NavigationCard
-          title="Recorder"
-          description="Use our built-in proxy to record a user flow"
+    <Flex direction="column" height="100%">
+      <ExperimentalBanner />
+      <Flex direction="column" align="center" justify="center" height="100%">
+        <Heading size="8" mb="3">
+          Discover what you can do with k6 Studio
+        </Heading>
+        <Text
+          css={css`
+            color: var(--gray-11);
+            font-size: 14px;
+            line-height: 22px;
+            max-width: 650px;
+            text-align: center;
+            margin-bottom: var(--space-6);
+          `}
         >
-          <Button variant="ghost" onClick={handleNavigateToRecorder}>
-            <DiscIcon />
-            Record flow
-          </Button>
-        </NavigationCard>
-        <NavigationCard
-          title="Generator"
-          description="Transform a recorded flow into a k6 test script"
-        >
-          <Button variant="ghost" onClick={handleCreateTestGenerator}>
-            <PlusCircledIcon />
-            Generate test
-          </Button>
-        </NavigationCard>
-        <NavigationCard
-          title="Validator"
-          description="Debug and validate your k6 script"
-        >
-          <Button variant="ghost" onClick={handleNavigateToValidator}>
-            <CheckCircledIcon /> Validate script
-          </Button>
-        </NavigationCard>
-      </Grid>
+          Uncover bottlenecks, accelerate load times, and deliver exceptional
+          user experiences with the industry{"'"}s leading performance testing
+          tool.
+        </Text>
+        <Grid gap="8" columns="3" maxWidth="720px">
+          <NavigationCard
+            title="Recorder"
+            description="Use our built-in proxy to record a user flow"
+          >
+            <Button variant="ghost" onClick={handleNavigateToRecorder}>
+              <DiscIcon />
+              Record flow
+            </Button>
+          </NavigationCard>
+          <NavigationCard
+            title="Generator"
+            description="Transform a recorded flow into a k6 test script"
+          >
+            <Button variant="ghost" onClick={handleCreateTestGenerator}>
+              <PlusCircledIcon />
+              Generate test
+            </Button>
+          </NavigationCard>
+          <NavigationCard
+            title="Validator"
+            description="Debug and validate your k6 script"
+          >
+            <Button variant="ghost" onClick={handleNavigateToValidator}>
+              <CheckCircledIcon /> Validate script
+            </Button>
+          </NavigationCard>
+        </Grid>
+      </Flex>
     </Flex>
   )
 }
