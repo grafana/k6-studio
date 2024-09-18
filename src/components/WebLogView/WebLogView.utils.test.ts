60d3f7876e53cea2b10e7ecb5003943d5e631f72
Cristiano Ventura <mrk.cristiano@gmail.com>
2024-09-18 16:15:48 -0400

    fix: join url parts with forward slash

---
+++ src/components/WebLogView/WebLogView.utils.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it } from 'vitest'
+import {
+  removeHostFromUrl,
+  removeProtocolFromUrl,
+  removeQueryStringFromUrl,
+} from './WebLogView.utils'
+
+describe('WebLogView.utils', () => {
+  it('should remove the host from the URL', () => {
+    const url = 'example.com/path/to/resource'
+    expect(removeHostFromUrl(url)).toBe('path/to/resource')
+  })
+
+  it('should remove the protocol from the URL', () => {
+    const url = 'https://example.com/path/to/resource'
+    expect(removeProtocolFromUrl(url)).toBe('example.com/path/to/resource')
+  })
+
+  it('should remove the query string from the URL', () => {
+    const url = 'https://example.com/path/to/resource?query=string'
+    expect(removeQueryStringFromUrl(url)).toBe(
+      'https://example.com/path/to/resource'
+    )
+  })
+})
