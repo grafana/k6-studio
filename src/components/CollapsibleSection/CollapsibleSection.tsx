eb31979759d80f0254ad1496d08f3f9b9b9f700d
Johan Suleiko Allansson <allansson@gmail.com>
2024-09-18 15:24:21 +0200

    revert CollapsibleSection

--- src/components/CollapsibleSection/CollapsibleSection.tsx
+++ src/components/CollapsibleSection/CollapsibleSection.tsx
@@ -1,106 +1,60 @@
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { CaretDownIcon, CaretRightIcon } from '@radix-ui/react-icons'
-import { Box, Flex, Reset } from '@radix-ui/themes'
-import { ReactNode, useState } from 'react'
+import { Flex, Reset } from '@radix-ui/themes'
+import { useState } from 'react'
 import './CollapsibleSection.styles.css'
 import { css } from '@emotion/react'
 
-interface CollapsibleSectionProps {
-  defaultOpen?: boolean
-  iconSize?: number
-  noTrigger?: boolean
-  content: ReactNode
-  actions?: ReactNode
-  children: ReactNode
-}
-
 export function CollapsibleSection({
+  children,
+  content,
   defaultOpen = false,
   iconSize = 20,
-  noTrigger = false,
-  content,
-  actions,
-  children,
-}: CollapsibleSectionProps) {
+}: {
+  children: React.ReactNode
+  content: React.ReactNode
+  defaultOpen?: boolean
+  iconSize?: number
+}) {
   const [open, setOpen] = useState(defaultOpen)
 
   return (
     <Collapsible.Root open={open} onOpenChange={setOpen}>
-      <div
-        css={css`
-          position: sticky;
-          top: 0;
-          z-index: 1;
-          display: flex;
-          align-items: center;
-          background-color: var(--gray-2);
-          border-top: 1px solid var(--gray-3);
-          border-bottom: 1px solid var(--gray-3);
-        `}
+      <Collapsible.Trigger
+        asChild
+        css={{
+          cursor: 'var(--cursor-button)',
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+        }}
       >
-        {noTrigger && (
-          <Box
+        <Reset>
+          <button
+            type="button"
             css={css`
-              flex: 1 1 0;
+              width: 100%;
+              background-color: var(--gray-2);
+              border-top: 1px solid var(--gray-3);
+              border-bottom: 1px solid var(--gray-3);
               padding: var(--space-2);
             `}
           >
-            <HeaderContent open={open} iconSize={iconSize}>
+            <Flex align="center" gap="1">
+              {open ? (
+                <CaretDownIcon height={iconSize} width={iconSize} />
+              ) : (
+                <CaretRightIcon height={iconSize} width={iconSize} />
+              )}
               {children}
-            </HeaderContent>
-          </Box>
-        )}
-        {!noTrigger && (
-          <Collapsible.Trigger
-            asChild
-            css={css`
-              flex: 1 1 0;
-              cursor: var(--cursor-button);
-            `}
-          >
-            <Reset>
-              <button
-                type="button"
-                css={css`
-                  padding: var(--space-2);
-                `}
-              >
-                <HeaderContent open={open} iconSize={iconSize}>
-                  {children}
-                </HeaderContent>
-              </button>
-            </Reset>
-          </Collapsible.Trigger>
-        )}
-        {actions && (
-          <Flex align="center" pr="3">
-            {actions}
-          </Flex>
-        )}
-      </div>
+            </Flex>
+          </button>
+        </Reset>
+      </Collapsible.Trigger>
 
       <Collapsible.Content className="CollapsibleContent">
         {content}
       </Collapsible.Content>
     </Collapsible.Root>
-  )
-}
-
-interface HeaderContentProps {
-  open: boolean
-  iconSize: number
-  children: ReactNode
-}
-
-function HeaderContent({ open, iconSize, children }: HeaderContentProps) {
-  return (
-    <Flex align="center" gap="1">
-      {open ? (
-        <CaretDownIcon height={iconSize} width={iconSize} />
-      ) : (
-        <CaretRightIcon height={iconSize} width={iconSize} />
-      )}
-      {children}
-    </Flex>
   )
 }
