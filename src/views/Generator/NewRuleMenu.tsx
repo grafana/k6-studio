cd18f212aee0ce62348227363bd0194f04c54d79
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-09-20 14:34:38 +0200

    adjust add rule button color

--- src/views/Generator/NewRuleMenu.tsx
+++ src/views/Generator/NewRuleMenu.tsx
@@ -17,7 +17,7 @@ export function NewRuleMenu(props: ComponentProps<typeof Button>) {
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <Button variant="ghost" size="1" color="orange" mr="2" {...props}>
+        <Button variant="ghost" size="1" color="gray" mr="2" {...props}>
           <PlusCircledIcon />
           Add rule
         </Button>
