e4003d70161db31f438beda77edd6ad27b44a3d3
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-09-23 11:13:53 +0200

    feat: Simplify Activity bar

--- src/components/Layout/ActivityBar/ActivityBar.tsx
+++ src/components/Layout/ActivityBar/ActivityBar.tsx
@@ -1,28 +1,14 @@
 import { css } from '@emotion/react'
-import {
-  Box,
-  DropdownMenu,
-  Flex,
-  Grid,
-  IconButton,
-  Text,
-  Tooltip,
-} from '@radix-ui/themes'
-import { Link, useMatch } from 'react-router-dom'
+import { Box, Flex, Grid, Text } from '@radix-ui/themes'
 
 import K6Logo from '@/assets/logo.svg'
 import { getRoutePath } from '@/routeMap'
 import { ThemeSwitcher } from '@/components/ThemeSwitcher'
 import { VersionLabel } from './VersionLabel'
 import { HomeIcon } from '@/components/icons'
-import { useCreateGenerator } from '@/hooks/useCreateGenerator'
 import { NavIconButton } from './NavIconButton'
-import { PlusIcon } from '@/components/icons/PlusIcon'
 
 export function ActivityBar() {
-  const createNewGenerator = useCreateGenerator()
-  const homeMatch = useMatch(getRoutePath('home'))
-
   return (
     <Flex direction="column" align="center" asChild position="relative">
       <Box
@@ -49,32 +35,7 @@ export function ActivityBar() {
             to={getRoutePath('home')}
             icon={<HomeIcon />}
             tooltip="Home"
-            active={Boolean(homeMatch)}
           />
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-              <div>
-                <Tooltip content="Actions" side="right">
-                  <IconButton variant="ghost" color="gray">
-                    <PlusIcon />
-                  </IconButton>
-                </Tooltip>
-              </div>
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content side="right">
-              <DropdownMenu.Item asChild>
-                <Link to={getRoutePath('recorder')} state={{ autoStart: true }}>
-                  Record flow
-                </Link>
-              </DropdownMenu.Item>
-              <DropdownMenu.Item onSelect={createNewGenerator}>
-                Generate test
-              </DropdownMenu.Item>
-              <DropdownMenu.Item asChild>
-                <Link to={getRoutePath('validator', {})}>Validate script</Link>
-              </DropdownMenu.Item>
-            </DropdownMenu.Content>
-          </DropdownMenu.Root>
         </Grid>
 
         <Flex direction="column" align="center" gap="3" mt="auto">
