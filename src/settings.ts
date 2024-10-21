808bbdd7bdbf6c5eae6e7a8bc2137509801e6a99
Cristiano Ventura <mrk.cristiano@gmail.com>
2024-10-21 14:36:30 -0400

    fix: parse result of all settings

--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,7 @@ import { app, dialog } from 'electron'
 import { writeFile, open } from 'fs/promises'
 import path from 'node:path'
 import { AppSettings } from './types/settings'
+import { AppSettingsSchema } from './schemas/appSettings'
 
 const defaultSettings: AppSettings = {
   version: '1.0',
@@ -44,16 +45,17 @@ async function initSettings() {
  * Retrieve  the current settings from the settings file
  * @returns The current settings as JSON
  */
-export async function getSettings(): Promise<AppSettings> {
+export async function getSettings() {
   await initSettings()
   const fileHandle = await open(filePath, 'r')
   try {
     const settings = await fileHandle?.readFile({ encoding: 'utf-8' })
     const currentSettings = JSON.parse(settings)
-    return {
+    const allSettings = {
       ...defaultSettings,
       ...currentSettings,
     }
+    return AppSettingsSchema.parse(allSettings)
   } finally {
     await fileHandle?.close()
   }
