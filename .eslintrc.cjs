511f31e6b3c328756cec257e0d45bac1e39fc4c0
Edgar Fisher <fisher.edgar@gmail.com>
2024-10-25 11:16:21 +0300

    chore: Add warnings about unsafe use of any

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/electron',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['resources/group_snippet.js'],
+  plugins: [
+    'import',
+    'unused-imports',
+    '@tanstack/query',
+    '@typescript-eslint',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'no-param-reassign': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    // TODO: remove warnings when plugin:@typescript-eslint/recommended-type-checked is enabled
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+  },
+
+  parserOptions: {
+    projectService: true,
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+}
