432c91a8a2efdc6436a73f8daf00c6f06bb4b997
Edgar Fisher <fisher.edgar@gmail.com>
2024-10-30 16:09:20 +0200

    fix: Show allowlist dialog as modal when action is required

--- src/views/Generator/Allowlist/Allowlist.tsx
+++ src/views/Generator/Allowlist/Allowlist.tsx
@@ -1,15 +1,18 @@
-import { Button } from '@radix-ui/themes'
+import { Button, Dialog, Flex } from '@radix-ui/themes'
 import { useGeneratorStore } from '@/store/generator'
 import { AllowlistDialog } from './AllowlistDialog'
 import { extractUniqueHosts } from '@/store/generator/slices/recording.utils'
 import { GlobeIcon } from '@radix-ui/react-icons'
 import { PopoverDialog } from '@/components/PopoverDialogs'
+import { useState } from 'react'
 
 export function Allowlist() {
   const requests = useGeneratorStore((store) => store.requests)
 
   const allowlist = useGeneratorStore((store) => store.allowlist)
   const setAllowlist = useGeneratorStore((store) => store.setAllowlist)
+
+  const [openAsPopover, setOpenAsPopover] = useState(false)
 
   const showAllowlistDialog = useGeneratorStore(
     (store) => store.showAllowlistDialog
@@ -27,33 +30,89 @@ export function Allowlist() {
 
   const hosts = extractUniqueHosts(requests)
 
+  function handleOpenChange(open: boolean) {
+    if (!open) {
+      setOpenAsPopover(false)
+    }
+
+    setShowAllowlistDialog(open)
+  }
+
+  // Show dialog as popover when triggered from the button
+  const Wrapper = openAsPopover ? PopoverWrapper : DialogWrapper
+
+  const trigger = (
+    <Button
+      size="1"
+      variant="ghost"
+      color="gray"
+      onClick={() => setOpenAsPopover(true)}
+    >
+      <GlobeIcon />
+      Allowed hosts [{allowlist.length}/{hosts.length}]
+    </Button>
+  )
+
   return (
-    <>
-      <PopoverDialog
-        open={showAllowlistDialog}
-        onOpenChange={setShowAllowlistDialog}
-        modal // needed to automatically open when switching recordings
-        trigger={
-          <Button
-            size="1"
-            variant="ghost"
-            color="gray"
-            onClick={() => setShowAllowlistDialog(true)}
-          >
-            <GlobeIcon />
-            Allowed hosts [{allowlist.length}/{hosts.length}]
-          </Button>
-        }
-      >
-        <AllowlistDialog
-          hosts={hosts}
-          allowlist={allowlist}
-          requests={requests}
-          includeStaticAssets={includeStaticAssets}
-          setAllowlist={setAllowlist}
-          setIncludeStaticAssets={setIncludeStaticAssets}
-        />
-      </PopoverDialog>
-    </>
+    <Wrapper
+      trigger={trigger}
+      open={showAllowlistDialog}
+      onOpenChange={handleOpenChange}
+    >
+      <AllowlistDialog
+        hosts={hosts}
+        allowlist={allowlist}
+        requests={requests}
+        includeStaticAssets={includeStaticAssets}
+        setAllowlist={setAllowlist}
+        setIncludeStaticAssets={setIncludeStaticAssets}
+      />
+    </Wrapper>
+  )
+}
+
+interface WrapperProps {
+  trigger: React.ReactNode
+  children: React.ReactNode
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
+
+function PopoverWrapper({
+  trigger,
+  children,
+  open,
+  onOpenChange,
+}: WrapperProps) {
+  return (
+    <PopoverDialog
+      open={open}
+      onOpenChange={onOpenChange}
+      trigger={trigger}
+      modal // needed to automatically open when switching recordings
+    >
+      {children}
+    </PopoverDialog>
+  )
+}
+
+function DialogWrapper({
+  trigger,
+  children,
+  open,
+  onOpenChange,
+}: WrapperProps) {
+  return (
+    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+      <Dialog.Trigger>{trigger}</Dialog.Trigger>
+      <Dialog.Content width="450px" size="2">
+        {children}
+        <Dialog.Close>
+          <Flex justify="end">
+            <Button>Continue</Button>
+          </Flex>
+        </Dialog.Close>
+      </Dialog.Content>
+    </Dialog.Root>
   )
 }
