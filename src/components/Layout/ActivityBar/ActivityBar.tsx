96fd906d90de27027a6ecbc9736072e70cb975f0
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2024-11-14 16:05:37 +0100

    chore: Update logo

--- src/components/Layout/ActivityBar/ActivityBar.tsx
+++ src/components/Layout/ActivityBar/ActivityBar.tsx
@@ -1,7 +1,7 @@
-import { css } from '@emotion/react'
-import { Box, Flex, Grid, Text } from '@radix-ui/themes'
+import { Box, Flex, Grid } from '@radix-ui/themes'
 
-import K6Logo from '@/assets/logo.svg'
+import k6Logo from '@/assets/logo.svg'
+import k6LogoDark from '@/assets/logo-dark.svg'
 import { getRoutePath } from '@/routeMap'
 import { VersionLabel } from './VersionLabel'
 import { HomeIcon } from '@/components/icons'
@@ -10,8 +10,11 @@ import { ApplicationLogButton } from './ApplicationLogButton'
 import { SettingsButton } from './SettingsButton'
 import { ProxyStatusIndicator } from './ProxyStatusIndicator'
 import { ThemeSwitcher } from '@/components/ThemeSwitcher'
+import { useTheme } from '@/hooks/useTheme'
 
 export function ActivityBar() {
+  const theme = useTheme()
+
   return (
     <Flex direction="column" align="center" asChild position="relative">
       <Box
@@ -22,18 +25,13 @@ export function ActivityBar() {
         overflow="hidden"
       >
         <Flex direction="column" align="center">
-          <img src={K6Logo} alt="k6 Logo" width="32" height="32" />
-          <Text
-            weight="bold"
-            css={css`
-              font-size: 10px;
-              cursor: default;
-            `}
-          >
-            Studio
-          </Text>
+          <img
+            src={theme === 'dark' ? k6LogoDark : k6Logo}
+            alt="k6 Logo"
+            width="32"
+          />
         </Flex>
-        <Grid gap="5" mt="6">
+        <Grid gap="5" mt="4">
           <NavIconButton
             to={getRoutePath('home')}
             icon={<HomeIcon />}
