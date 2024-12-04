8cbe1009e405e94f0e3d87d04452407581ff9f14
Cristiano Ventura <mrk.cristiano@gmail.com>
2024-12-04 11:13:20 -0500

    migrate generator v0 to v1

---
+++ src/schemas/generator/index.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+import * as v0 from './v0'
+import * as v1 from './v1'
+import { exhaustive } from '../../utils/typescript'
+
+const AnyGeneratorSchema = z.discriminatedUnion('version', [
+  v0.GeneratorFileDataSchema,
+  v1.GeneratorFileDataSchema,
+])
+
+export function migrate(generator: z.infer<typeof AnyGeneratorSchema>) {
+  switch (generator.version) {
+    case '0':
+      return migrate(v0.migrate(generator))
+    case '1.0':
+      return generator
+    default:
+      return exhaustive(generator)
+  }
+}
+
+export const GeneratorFileDataSchema = AnyGeneratorSchema.transform(migrate)
