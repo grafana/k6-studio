2b2a08a1a9ab7f2cc3022a7c2f88441e5d36867c
Cristiano Ventura <mrk.cristiano@gmail.com>
2025-01-02 13:35:03 -0500

    use k6StudioDarkBackground variable

--- src/components/Monaco/EditorToolbar.tsx
+++ src/components/Monaco/EditorToolbar.tsx
@@ -4,6 +4,7 @@ import { k6StudioLightBackground } from './themes/k6StudioLight'
 import { useTheme } from '@/hooks/useTheme'
 import { useLocalStorage } from 'react-use'
 import { WordWrapIcon } from '../icons'
+import { k6StudioDarkBackground } from './themes/k6StudioDark'
 
 export type ToolbarState = {
   wordWrap: 'on' | 'off'
@@ -34,7 +35,8 @@ export const EditorToolbar = ({ getState, actions }: EditorToolbarProps) => {
       p="2"
       justify="end"
       style={{
-        backgroundColor: theme === 'dark' ? '#1e1e1e' : k6StudioLightBackground,
+        backgroundColor:
+          theme === 'dark' ? k6StudioDarkBackground : k6StudioLightBackground,
         borderBottom: `1px solid ${theme === 'dark' ? 'var(--gray-6)' : 'var(--gray-4)'}`,
       }}
     >
