37a1470006b1ec7b9dd5b2076339ef988220fc46
Johan Suleiko Allansson <allansson@gmail.com>
2025-02-18 14:57:46 +0100

    add some comments to document the different states

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -7,42 +7,72 @@ export interface Stack {
   archived: boolean
 }
 
+/**
+ * Initial state of sign-in where the OAuth server is contacted to
+ * get the user code.
+ */
 export interface InitializingState {
   type: 'initializing'
 }
 
+/**
+ * The app is waiting for the user to complete authorization in the browser.
+ */
 export interface AwaitingAuthorizationState {
   type: 'awaiting-authorization'
   code: string
 }
 
+/**
+ * The user denied the authorization request.
+ */
 export interface AuthorizationDeniedState {
   type: 'authorization-denied'
 }
 
+/**
+ * Entered when the user is already signed in with a different account.
+ */
 export interface SignOutRequiredState {
   type: 'sign-out-required'
 }
 
+/**
+ * Loading state where we're fetching the available stacks from the
+ * GCOM API.
+ */
 export interface FetchingStacksState {
   type: 'fetching-stacks'
 }
 
+/**
+ * State where we're waiting for the user to select a stack to sign-in to.
+ */
 export interface SelectingStackState {
   type: 'selecting-stack'
   stacks: Stack[]
 }
 
+/**
+ * Loading state where we're fetching the personal API token from the k6 API.
+ */
 export interface FetchingTokenState {
   type: 'fetching-token'
   stack: Stack
 }
 
+/**
+ * Users are not synced until they sign-in to a stack. This state is entered
+ * when the user needs to sign-in to a stack to continue.
+ */
 export interface StackLoginRequiredState {
   type: 'stack-login-required'
   stack: Stack
 }
 
+/**
+ * A time out occurred while waiting for the user to sign-in.
+ */
 export interface TimedOutState {
   type: 'timed-out'
 }
