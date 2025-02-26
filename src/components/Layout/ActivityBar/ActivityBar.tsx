27d9ce0abbdf69db68de6c7796f4a95134790e57
Johan Suleiko Allansson <allansson@gmail.com>
2025-02-26 15:51:46 +0100

    Revert "temporarily enable feature for alpha build"
    This reverts commit 610a3b3fe455bfc87a57d1b491cb863bd664e416.

--- src/components/Layout/ActivityBar/ActivityBar.tsx
+++ src/components/Layout/ActivityBar/ActivityBar.tsx
@@ -13,6 +13,7 @@ import { ThemeSwitcher } from '@/components/ThemeSwitcher'
 import { useTheme } from '@/hooks/useTheme'
 import { Profile } from './Profile'
 import { css } from '@emotion/react'
+import { Feature } from '@/components/Feature'
 
 export function ActivityBar() {
   const theme = useTheme()
@@ -57,9 +58,9 @@ export function ActivityBar() {
         </Flex>
         <Separator orientation="horizontal" size="2" />
         <Flex direction="column" align="center" gap="3">
-          {/* <Feature feature="cloud-auth"> */}
-          <Profile />
-          {/* </Feature> */}
+          <Feature feature="cloud-auth">
+            <Profile />
+          </Feature>
           <VersionLabel />
         </Flex>
       </Flex>
