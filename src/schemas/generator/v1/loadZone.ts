e5edc2bd8ae81373f5a60743ab0d4f6b05db9759
Cristiano Ventura <mrk.cristiano@gmail.com>
2025-03-04 13:08:54 -0500

    show current percentage value in error

--- src/schemas/generator/v1/loadZone.ts
+++ src/schemas/generator/v1/loadZone.ts
@@ -27,7 +27,11 @@ export const AvailableLoadZonesSchema = z.enum([
 export const LoadZoneItemSchema = z.object({
   id: z.string(),
   loadZone: AvailableLoadZonesSchema,
-  percent: z.number().int().min(1, { message: 'Invalid percentage' }).max(100),
+  percent: z
+    .number({ message: 'Invalid percentage' })
+    .int()
+    .min(1, { message: 'Invalid percentage' })
+    .max(100, { message: 'Invalid percentage' }),
 })
 
 export const LoadZoneSchema = z.object({
@@ -38,12 +42,19 @@ export const LoadZoneSchema = z.object({
         return true
       }
 
-      const totalPercentage = data.reduce(
-        (sum, { percent }) => sum + percent,
-        0
-      )
+      const totalPercentage = currentLoadZonePercentage(data)
       return totalPercentage === 100
     },
-    { message: 'The sum of all distribution percentages must be 100' }
+    (data) => {
+      const totalPercentage = currentLoadZonePercentage(data)
+      return {
+        message: `Total percentage must be 100, currently ${totalPercentage}`,
+        path: ['root'],
+      }
+    }
   ),
 })
+
+function currentLoadZonePercentage(data: { percent: number }[]) {
+  return data.reduce((sum, { percent }) => sum + percent, 0)
+}
