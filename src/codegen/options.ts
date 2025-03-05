49fc6311c8c44cf76cb68355154f12cf81035955
Cristiano Ventura <mrk.cristiano@gmail.com>
2025-03-05 11:05:42 -0500

    generate cloud options

--- src/codegen/options.ts
+++ src/codegen/options.ts
@@ -1,14 +1,16 @@
-import { TestOptions, Threshold } from '@/types/testOptions'
+import { LoadZoneData, TestOptions, Threshold } from '@/types/testOptions'
 import { stringify } from './codegen.utils'
 import { omit } from 'lodash-es'
 
 export function generateOptions({
   loadProfile,
   thresholds,
+  cloud,
 }: TestOptions): string {
   const options = omit(loadProfile, ['executor'])
   const data = {
     ...options,
+    ...(cloud && generateCloudOptions(cloud)),
     ...(thresholds.length > 0 && {
       thresholds: generateThresholds(thresholds),
     }),
@@ -38,6 +40,26 @@ export function generateThresholds(thresholds: Threshold[]) {
     } else {
       result[key].push(thresholdValue)
     }
+  })
+
+  return result
+}
+
+export function generateCloudOptions({ loadZones }: TestOptions['cloud']) {
+  if (loadZones.loadZones.length === 0) return {}
+
+  return {
+    cloud: {
+      distribution: generateLoadZones(loadZones),
+    },
+  }
+}
+
+export function generateLoadZones({ loadZones }: LoadZoneData) {
+  const result: Record<string, { loadZone: string; percent: number }> = {}
+
+  loadZones.forEach(({ loadZone, percent }) => {
+    result[`'${loadZone}'`] = { loadZone, percent }
   })
 
   return result
