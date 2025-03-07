6cda70e7b54c101bcae97f34ce9029fbfdf5640c
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2025-03-07 16:44:11 +0100

    fix: Renaming generator with unsaved changes creates extra file

--- src/components/FileTree/File.hooks.ts
+++ src/components/FileTree/File.hooks.ts
@@ -1,16 +1,20 @@
 import { useStudioUIStore } from '@/store/ui'
 import { StudioFile } from '@/types'
-import { getFileNameWithoutExtension } from '@/utils/file'
+import { getFileNameWithoutExtension, getViewPath } from '@/utils/file'
+import { queryClient } from '@/utils/query'
 import { useMutation } from '@tanstack/react-query'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export function useRenameFile(file: StudioFile) {
+  const { fileName: selectedFileName } = useParams()
+  const navigate = useNavigate()
   const addFile = useStudioUIStore((state) => state.addFile)
   const removeFile = useStudioUIStore((state) => state.removeFile)
 
   return useMutation({
     mutationFn: (newName: string) =>
       window.studio.ui.renameFile(file.fileName, newName, file.type),
-    onSuccess: (_, newName) => {
+    onSuccess: async (_, newName) => {
       // There's a slight delay between the add and remove callbacks being triggered,
       // causing the UI to flicker because it thinks the renamed file is actually
       // a new file. To prevent this, we optimistically update the file list.
@@ -19,8 +23,26 @@ export function useRenameFile(file: StudioFile) {
         displayName: getFileNameWithoutExtension(newName),
         fileName: newName,
       }
+
       removeFile(file)
       addFile(updatedFile)
+
+      if (file.type === 'generator') {
+        queryClient.setQueryData(
+          ['generator', newName],
+          queryClient.getQueryData(['generator', file.fileName])
+        )
+      }
+
+      if (selectedFileName === file.fileName) {
+        navigate(getViewPath(file.type, newName), { replace: true })
+      }
+
+      if (file.type === 'generator') {
+        await queryClient.invalidateQueries({
+          queryKey: ['generator', file.fileName],
+        })
+      }
     },
   })
 }
