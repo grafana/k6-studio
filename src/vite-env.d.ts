337df27e7db61d40aeb44cf73d588226066a2679
Johan Suleiko Allansson <allansson@gmail.com>
2025-03-10 09:34:18 +0100

    remove mock token

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,6 @@
 declare const __APP_VERSION__: string
 
 declare const K6_API_URL: string
-declare const MOCK_PERSONAL_API_TOKEN: string
 declare const GRAFANA_CLIENT_ID: string
 declare const GRAFANA_API_URL: string
 declare const GRAFANA_COM_URL: string
