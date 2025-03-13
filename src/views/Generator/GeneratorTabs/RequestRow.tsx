b99c4009770a200669e06e34bc776e09c12ff994
Edgar Fisher <fisher.edgar@gmail.com>
2025-03-13 17:10:40 +0200

    feat: use labels to highlight matched requests

--- src/views/Generator/GeneratorTabs/RequestRow.tsx
+++ src/views/Generator/GeneratorTabs/RequestRow.tsx
@@ -1,26 +1,54 @@
-import { Row, RowProps } from '@/components/WebLogView'
+import { Cells, RowProps } from '@/components/WebLogView'
+import { SearchResults } from '@/components/WebLogView/SearchResults'
+import { Badge, Flex, Strong, Table } from '@radix-ui/themes'
 import { useMemo } from 'react'
 
-export function RequestRow(
-  props: RowProps & { highlightedRequestIds?: string[] }
-) {
-  const opacity = useMemo(() => {
-    if (!props.highlightedRequestIds) {
-      return 1
+export function RequestRow({
+  data,
+  onSelectRequest,
+  isSelected,
+  filter,
+  highlightedRequestIds,
+}: RowProps & { highlightedRequestIds?: string[] }) {
+  const isMatch = useMemo(() => {
+    if (!highlightedRequestIds) {
+      return false
     }
 
-    return props.highlightedRequestIds.includes(props.data.id) ? 1 : 0.4
-  }, [props.highlightedRequestIds, props.data.id])
+    return highlightedRequestIds.includes(data.id)
+  }, [highlightedRequestIds, data.id])
   return (
-    <Row
-      {...props}
-      css={{
-        opacity,
-        td: {
-          // Match border color with hihglighted rows
-          boxShadow: `inset 0 -1px var(--gray-${opacity === 1 ? 'a5' : 'a8'})`,
-        },
-      }}
-    />
+    <>
+      <Table.Row
+        onClick={() => onSelectRequest(data)}
+        css={{
+          backgroundColor: isSelected ? 'var(--accent-3)' : 'transparent',
+          '&:hover': {
+            backgroundColor: isSelected ? 'var(--accent-3)' : 'var(--accent-2)',
+          },
+        }}
+      >
+        <Cells data={data} isSelected={isSelected} />
+        {highlightedRequestIds && (
+          <Table.Cell css={{ padding: 0 }}>
+            {isMatch && (
+              <Flex justify="end" align="center" height="100%" pr="2">
+                <Badge color="green" size="1">
+                  <Strong>Match</Strong>
+                </Badge>
+              </Flex>
+            )}
+          </Table.Cell>
+        )}
+      </Table.Row>
+
+      <SearchResults
+        data={data}
+        key={data.id}
+        onSelectRequest={onSelectRequest}
+        filter={filter}
+        colSpan={highlightedRequestIds ? 6 : 5}
+      />
+    </>
   )
 }
