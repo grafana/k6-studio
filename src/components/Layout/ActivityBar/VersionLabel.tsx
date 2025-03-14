495b2e82b91227069619b674c0c70aac0260ee6e
Llandy Riveron Del Risco <llandy3d@gmail.com>
2025-03-14 15:53:24 +0100

    Revert "temp version setting"
    This reverts commit bb45091032f688d86eb2f82a93f2a5a777a91c10.

--- src/components/Layout/ActivityBar/VersionLabel.tsx
+++ src/components/Layout/ActivityBar/VersionLabel.tsx
@@ -4,7 +4,7 @@ export function VersionLabel() {
   return (
     <Flex direction="column" gap="1" align="center">
       <Code size="1" variant="ghost" color="gray">
-        v1.0.0
+        v{__APP_VERSION__}
       </Code>
     </Flex>
   )
