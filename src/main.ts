26177fc1c4163f8ffa2c321ae27999d94c82cb54
Cristiano Ventura <mrk.cristiano@gmail.com>
2025-04-02 17:25:45 -0400

    Move createFileWithUniqueName helper function

--- src/main.ts
+++ src/main.ts
@@ -69,6 +69,7 @@ import {
   getPlatform,
   browserWindowFromEvent,
 } from './utils/electron'
+import { createFileWithUniqueName } from './utils/fileSystem'
 import { createNewGeneratorFile } from './utils/generator'
 import { exhaustive, isNodeJsErrnoException } from './utils/typescript'
 import { setupProjectStructure } from './utils/workspace'
@@ -998,44 +999,4 @@ const cleanUpProxies = async () => {
   processList.forEach((proc) => {
     kill(proc.pid)
   })
-}
-
-const createFileWithUniqueName = async ({
-  directory,
-  data,
-  prefix,
-  ext,
-}: {
-  directory: string
-  data: string
-  prefix: string
-  ext: string
-}): Promise<string> => {
-  const timestamp = new Date().toISOString().split('T')[0] ?? ''
-  const template = `${prefix ? `${prefix} - ` : ''}${timestamp}${ext}`
-
-  // Start from 2 as it follows the the OS behavior for duplicate files
-  let fileVersion = 2
-  let uniqueFileName = template
-  let fileCreated = false
-
-  do {
-    try {
-      // ax+ flag will throw an error if the file already exists
-      await writeFile(path.join(directory, uniqueFileName), data, {
-        flag: 'ax+',
-      })
-      fileCreated = true
-    } catch (error) {
-      if (isNodeJsErrnoException(error) && error.code !== 'EEXIST') {
-        throw error
-      }
-
-      const { name, ext } = path.parse(template)
-      uniqueFileName = `${name} (${fileVersion})${ext}`
-      fileVersion++
-    }
-  } while (!fileCreated)
-
-  return uniqueFileName
 }
