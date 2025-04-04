f40d5e34a7a5a29481e95c08b2a08e3b2a671bd1
Cristiano Ventura <mrk.cristiano@gmail.com>
2025-04-04 18:40:54 -0400

    Move browser preload

--- src/handlers/browser/types.ts
+++ src/handlers/browser/types.ts
@@ -1,5 +1,7 @@
 export enum BrowserHandler {
   Start = 'browser:start',
   Stop = 'browser:stop',
+  Closed = 'browser:closed',
+  Failed = 'browser:failed',
   OpenExternalLink = 'browser:open:external:link',
 }
