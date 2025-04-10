f4cb277fae821500e9773e27a070eb4b8638b617
Llandy Riveron Del Risco <llandy3d@gmail.com>
2025-04-10 13:56:44 +0200

    chore: release test version action (#644)

---
+++ update_version.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+
+version = os.environ['INPUT_VERSION']
+
+
+with open('package.json', 'r') as f:
+    data = json.loads(f.read())
+
+
+with open('package.json', 'w') as f:
+    data['version'] = version
+    f.write(json.dumps(data, indent=2))
+
+print(f'Updated package.json version to {version}')
