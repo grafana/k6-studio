d25e6db602a5ba680a4d7ee77b68dd674b6cd8fa
Johan Suleiko Allansson <allansson@gmail.com>
2025-04-10 09:53:15 +0200

    better visibility when highlighting elements

--- extension/src/frontend/view/ElementHighlight.tsx
+++ extension/src/frontend/view/ElementHighlight.tsx
@@ -16,9 +16,18 @@ export const ElementHighlight = forwardRef<
     <Overlay
       ref={ref}
       css={css`
-        background-color: rgba(0, 0, 0, 0.1);
-        border: 1px solid rgba(0, 0, 0);
         z-index: var(--studio-layer-0);
+        outline: 2px solid var(--gray-12);
+
+        &:after {
+          content: ' ';
+          display: block;
+          position: absolute;
+          inset: 0;
+          // This will darken everything around the highlighted element
+          // giving a sense of a cutout.
+          outline: 10000px solid rgba(0, 0, 0, 0.4);
+        }
       `}
       bounds={bounds}
     />
