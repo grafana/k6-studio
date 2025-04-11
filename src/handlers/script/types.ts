a6c092928acbe6e8b2b406773d6fda3d66f0bd8e
Cristiano Ventura <mrk.cristiano@gmail.com>
2025-04-11 09:27:29 -0400

    refactor(split main): Move script handlers out of main.ts (#642)

---
+++ src/handlers/script/types.ts
@@ -0,0 +1,13 @@
+export enum ScriptHandler {
+  Select = 'script:select',
+  Open = 'script:open',
+  Run = 'script:run',
+  Stop = 'script:stop',
+  Save = 'script:save',
+  Log = 'script:log',
+  Stopped = 'script:stopped',
+  Finished = 'script:finished',
+  Failed = 'script:failed',
+  Check = 'script:check',
+  RunFromGenerator = 'script:run-from-generator',
+}
