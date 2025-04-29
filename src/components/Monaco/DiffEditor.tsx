14411bf82df2e6e71cddfbe3b6242506f5653e8b
Edgar Fisher <fisher.edgar@gmail.com>
2025-04-29 10:00:50 +0300

    fix: hide minimap in diff editor, enable highlighting

--- src/components/Monaco/DiffEditor.tsx
+++ src/components/Monaco/DiffEditor.tsx
@@ -5,26 +5,35 @@ import { ComponentProps } from 'react'
 import { useTheme } from '@/hooks/useTheme'
 
 import { DEFAULT_OPTIONS } from './defaultOptions'
-const options: editor.IStandaloneEditorConstructionOptions = {
+const DEFAULT_DIFF_OPTIONS: editor.IDiffEditorOptions = {
   ...DEFAULT_OPTIONS,
   readOnly: true,
+  renderOverviewRuler: false,
+  lineNumbers: 'off',
 }
 
 interface Props extends ComponentProps<typeof Editor> {
   original?: string
 }
 
-export function DiffEditor(props: Props) {
+export function DiffEditor({ original, value, language, options }: Props) {
   const theme = useTheme()
   return (
     <MonacoDiffEditor
       options={{
+        ...DEFAULT_DIFF_OPTIONS,
         ...options,
-        ...props.options,
       }}
-      original={props.original}
-      modified={props.value}
+      language={language}
+      original={original}
+      modified={value}
       theme={theme === 'dark' ? 'k6-studio-dark' : 'k6-studio-light'}
+      css={{
+        // Hide lightbulb icon in diff editor
+        '.codicon': {
+          display: 'none',
+        },
+      }}
     />
   )
 }
