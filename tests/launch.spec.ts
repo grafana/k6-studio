3fcb66094aa02bd22373c0ea9ae8db3867a896b8
Llandy Riveron Del Risco <llandy3d@gmail.com>
2025-06-10 12:12:16 +0200

    fix: remove awaits and change import type

--- tests/launch.spec.ts
+++ tests/launch.spec.ts
@@ -1,14 +1,10 @@
-const { ElectronApplication, Page, test, expect, _electron: electron } = require('@playwright/test')
-import {
-  findLatestBuild,
-  parseElectronApp,
-} from 'electron-playwright-helpers'
-
+import { test, expect, _electron as electron } from '@playwright/test'
+import type { ElectronApplication, Page } from '@playwright/test'
+import { findLatestBuild, parseElectronApp } from 'electron-playwright-helpers'
 
 let electronApp: ElectronApplication
 let mainWindow: Page
 let splashscreenWindow: Page
-
 
 test.beforeAll(async () => {
   const latestBuild = findLatestBuild()
@@ -16,7 +12,7 @@ test.beforeAll(async () => {
 
   electronApp = await electron.launch({
     args: [appInfo.main],
-    executablePath: appInfo.executable
+    executablePath: appInfo.executable,
   })
 
   // splashscreen
@@ -32,16 +28,15 @@ test('launch app', async () => {
   const title = await mainWindow.title()
   expect(title).toBe('Grafana k6 Studio')
 
-  const body = await splashscreenWindow.locator('body');
+  const body = splashscreenWindow.locator('body')
   // not loading without this await for some reason
   await body.count()
   expect(body).toBeVisible()
 })
 
-
 test('start recording', async () => {
   const testUrl = 'quickpizza.grafana.com'
-  const recordingButton = await mainWindow.getByRole('link', { name: /record flow/i })
+  const recordingButton = mainWindow.getByRole('link', { name: /record flow/i })
   await recordingButton.click()
 
   // insert test url
@@ -49,26 +44,30 @@ test('start recording', async () => {
   await urlInput.fill(testUrl)
 
   // start recording button
-  const startRecording = await mainWindow.getByRole('button', { name: /start recording/i })
+  const startRecording = mainWindow.getByRole('button', {
+    name: /start recording/i,
+  })
   expect(startRecording).toBeVisible()
 
   await startRecording.click()
 
-
   // requests are getting recorded, check row appears with quickpizza
-  const pizzaRows = await mainWindow.locator('tr:has-text("quickpizza.grafana.com")')
+  const pizzaRows = mainWindow.locator('tr:has-text("quickpizza.grafana.com")')
   await pizzaRows.first().waitFor()
 
   expect(pizzaRows.first()).toBeVisible()
 
-
   // stop recording
-  const stopRecordingButton = await mainWindow.getByRole('button', { name: /Stop recording/i })
+  const stopRecordingButton = mainWindow.getByRole('button', {
+    name: /Stop recording/i,
+  })
   expect(stopRecordingButton).toBeVisible()
   await stopRecordingButton.click()
 
   // assert we have the create test generator button
-  const createTestGeneratorButton = await mainWindow.getByRole('button', { name: /Create test generator/i })
+  const createTestGeneratorButton = mainWindow.getByRole('button', {
+    name: /Create test generator/i,
+  })
   await createTestGeneratorButton.waitFor()
   expect(createTestGeneratorButton).toBeVisible()
 })
