f7ad41c55ae3a983eee5820d0b6153afa94b52ff
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2025-06-13 10:50:50 +0200

    wip

--- src/services/usageTracking/types.ts
+++ src/services/usageTracking/types.ts
@@ -6,19 +6,14 @@ export enum UsageTrackingEvents {
 
   // Recorder
   RecordingCreated = 'recording_created',
-  RecordingImported = 'recording_imported',
 
   // Generator
   GeneratorSaved = 'generator_saved',
 
   // Script
   ScriptExported = 'script_exported',
-  ScriptOpened = 'script_opened',
   ScriptValidated = 'script_validated',
   ScriptRunInCloud = 'script_run_in_cloud',
-
-  // User
-  UserAuthenticated = 'user_authenticated',
 }
 
 export interface UsageTrackingEventMetadata {
@@ -27,8 +22,6 @@ export interface UsageTrackingEventMetadata {
   appVersion: string
   os: Platform
   arch: Arch
-  userId?: string
-  orgId?: string
 }
 
 export interface AppInstalledEvent {
@@ -47,10 +40,6 @@ export interface ScriptExportedEvent {
   type: UsageTrackingEvents.ScriptExported
 }
 
-export interface ScriptOpenedEvent {
-  type: UsageTrackingEvents.ScriptOpened
-}
-
 export interface ScriptValidatedEvent {
   type: UsageTrackingEvents.ScriptValidated
 }
@@ -59,20 +48,13 @@ export interface ScriptRunInCloudEvent {
   type: UsageTrackingEvents.ScriptRunInCloud
 }
 
-export interface UserAuthenticatedEvent {
-  type: UsageTrackingEvents.UserAuthenticated
-}
-
 export type UsageTrackingEvent =
   | AppInstalledEvent
   | RecordingCreatedEvent
   | GeneratorCreatedEvent
   | ScriptExportedEvent
-  | ScriptOpenedEvent
   | ScriptValidatedEvent
   | ScriptRunInCloudEvent
-  | UserAuthenticatedEvent
 
-export type UsageTrackingEventWithMetadata = UsageTrackingEvent & {
-  metadata: UsageTrackingEventMetadata
-}
+export type UsageTrackingEventWithMetadata = UsageTrackingEvent &
+  UsageTrackingEventMetadata
