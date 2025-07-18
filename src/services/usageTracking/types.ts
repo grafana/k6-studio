fc424c465f552da87f83dc04c54fd67b0c01cb8a
Uladzimir Dzmitrachkou <uladzimir.dzmitrachkou@grafana.com>
2025-07-18 15:50:53 +0200

    chore: Anonymous usage tracking for Recorder, Generator, Validator

--- src/services/usageTracking/types.ts
+++ src/services/usageTracking/types.ts
@@ -3,6 +3,19 @@ import { Arch, Platform } from '@/types/electron'
 export enum UsageEventName {
   // General
   AppInstalled = 'app_installed',
+
+  // Recorder
+  RecordingCreated = 'recording_created',
+  RecordingImported = 'recording_imported',
+
+  // Generator
+  GeneratorCreated = 'generator_created',
+  GeneratorUpdated = 'generator_updated',
+
+  // Script
+  ScriptExported = 'script_exported',
+  ScriptValidated = 'script_validated',
+  ScriptRunInCloud = 'script_run_in_cloud',
 }
 
 export interface UsageEventMetadata {
@@ -17,6 +30,42 @@ export interface AppInstalledEvent {
   event: UsageEventName.AppInstalled
 }
 
-export type UsageEvent = AppInstalledEvent
+export interface RecordingCreatedEvent {
+  event: UsageEventName.RecordingCreated
+}
+
+export interface RecordingImportedEvent {
+  event: UsageEventName.RecordingImported
+}
+
+export interface GeneratorCreatedEvent {
+  event: UsageEventName.GeneratorCreated
+}
+
+export interface GeneratorUpdatedEvent {
+  event: UsageEventName.GeneratorUpdated
+}
+
+export interface ScriptExportedEvent {
+  event: UsageEventName.ScriptExported
+}
+
+export interface ScriptValidatedEvent {
+  event: UsageEventName.ScriptValidated
+}
+
+export interface ScriptRunInCloudEvent {
+  event: UsageEventName.ScriptRunInCloud
+}
+
+export type UsageEvent =
+  | AppInstalledEvent
+  | RecordingCreatedEvent
+  | RecordingImportedEvent
+  | GeneratorCreatedEvent
+  | GeneratorUpdatedEvent
+  | ScriptExportedEvent
+  | ScriptValidatedEvent
+  | ScriptRunInCloudEvent
 
 export type UsageEventWithMetadata = UsageEvent & UsageEventMetadata
