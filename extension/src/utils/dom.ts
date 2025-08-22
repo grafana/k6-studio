9740294c738b8ad27d815308b9fb31683ad4a803
Johan Suleiko Allansson <allansson@gmail.com>
2025-08-22 11:25:36 +0200

    use non-recursive implementation

--- extension/src/utils/dom.ts
+++ extension/src/utils/dom.ts
@@ -22,37 +22,43 @@ const SIMPLE_CLICK_WIDGET_ROLES = [
 ]
 
 export function findInteractiveElement(element: Element): Element | null {
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Guides/Content_categories#interactive_content
-  if (
-    element instanceof HTMLButtonElement ||
-    element instanceof HTMLLabelElement ||
-    element instanceof HTMLTextAreaElement ||
-    element instanceof HTMLSelectElement
-  ) {
-    return element
+  let current: Element | null = element
+
+  while (element !== null) {
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Guides/Content_categories#interactive_content
+    if (
+      element instanceof HTMLButtonElement ||
+      element instanceof HTMLLabelElement ||
+      element instanceof HTMLTextAreaElement ||
+      element instanceof HTMLSelectElement
+    ) {
+      return element
+    }
+
+    if (element instanceof HTMLAnchorElement && element.hasAttribute('href')) {
+      return element
+    }
+
+    if (element instanceof HTMLImageElement && element.hasAttribute('usemap')) {
+      return element
+    }
+
+    if (element instanceof HTMLInputElement && element.type !== 'hidden') {
+      return element
+    }
+
+    const role = element.getAttribute('role')
+
+    if (role !== null && SIMPLE_CLICK_WIDGET_ROLES.includes(role)) {
+      return element
+    }
+
+    if (element.parentElement === null) {
+      return null
+    }
+
+    current = element.parentElement
   }
 
-  if (element instanceof HTMLAnchorElement && element.hasAttribute('href')) {
-    return element
-  }
-
-  if (element instanceof HTMLImageElement && element.hasAttribute('usemap')) {
-    return element
-  }
-
-  if (element instanceof HTMLInputElement && element.type !== 'hidden') {
-    return element
-  }
-
-  const role = element.getAttribute('role')
-
-  if (role !== null && SIMPLE_CLICK_WIDGET_ROLES.includes(role)) {
-    return element
-  }
-
-  if (element.parentElement === null) {
-    return null
-  }
-
-  return findInteractiveElement(element.parentElement)
+  return current
 }
