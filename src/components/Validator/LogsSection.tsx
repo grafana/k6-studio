fee01e1d71638bcfaae81ac5abf7360a2ed08b71
Johan Suleiko Allansson <allansson@gmail.com>
2025-09-12 16:05:48 +0200

    use table in logs panel

--- src/components/Validator/LogsSection.tsx
+++ src/components/Validator/LogsSection.tsx
@@ -1,22 +1,107 @@
-import { ScrollArea } from '@radix-ui/themes'
+import { css } from '@emotion/react'
+import { Text } from '@radix-ui/themes'
 
-import { LogView } from '@/components/LogView'
 import { useAutoScroll } from '@/hooks/useAutoScroll'
-import { K6Log } from '@/types'
+import { LogEntry } from '@/schemas/k6'
 
-interface LogsSectionProps {
-  logs: K6Log[]
-  autoScroll?: boolean
+import { Table } from '../Table'
+
+function findTableElement(element: HTMLElement): HTMLTableElement | null {
+  let current: HTMLElement | null = element
+
+  while (current !== null) {
+    if (current instanceof HTMLTableElement) {
+      return current
+    }
+
+    current = current.parentElement
+  }
+
+  return null
 }
 
-export function LogsSection({ logs, autoScroll = true }: LogsSectionProps) {
-  const ref = useAutoScroll(logs, autoScroll)
+const colors: Record<LogEntry['level'], string> = {
+  info: 'green',
+  debug: 'blue',
+  warning: 'orange',
+  error: 'red',
+}
+
+const header = css`
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  --table-row-background-color: var(--color-background);
+`
+
+interface LogsSectionProps {
+  logs: LogEntry[]
+  autoScroll: boolean
+}
+
+export function LogsSection({ logs, autoScroll }: LogsSectionProps) {
+  const ref = useAutoScroll<HTMLTableElement>(logs, autoScroll)
+
+  // Radix UI's Table component wraps the table element in a ScrollArea but in
+  // order to autoscroll we need to get a ref to the table element. Ideally Radix
+  // would provide a way to do this, but instead we have to get the ref to an
+  // inner element and find the table element from there.
+  const getTableElement = (element: HTMLTableSectionElement | null) => {
+    if (element === null) {
+      return
+    }
+
+    const table = findTableElement(element)
+
+    if (table === null) {
+      return
+    }
+
+    ref.current = table
+  }
 
   return (
-    <ScrollArea scrollbars="vertical">
-      <div ref={ref}>
-        <LogView logs={logs} />
-      </div>
-    </ScrollArea>
+    <Table.Root
+      size="1"
+      layout="fixed"
+      css={css`
+        height: 100%;
+
+        td {
+          font-family: var(--code-font-family);
+        }
+      `}
+    >
+      <Table.Header ref={getTableElement}>
+        <Table.Row>
+          <Table.ColumnHeaderCell css={header} width="230px">
+            Time
+          </Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell css={header}>Message</Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {logs.map((log, index) => (
+          <Table.Row key={index}>
+            <Table.Cell
+              css={css`
+                border-left: 3px solid var(--${colors[log.level]}-9);
+              `}
+            >
+              <Text>{log.time}</Text>
+            </Table.Cell>
+            <Table.Cell>
+              <pre
+                css={css`
+                  margin: 0;
+                `}
+              >
+                {log.msg}
+              </pre>
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
   )
 }
