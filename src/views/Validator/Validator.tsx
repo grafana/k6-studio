52aa12e4ad4533826ff1915a2a6f110963f02273
Johan Suleiko Allansson <allansson@gmail.com>
2025-09-19 15:19:59 +0200

    network/console/checks drawer

--- src/views/Validator/Validator.tsx
+++ src/views/Validator/Validator.tsx
@@ -1,25 +1,8 @@
 import { css } from '@emotion/react'
-import {
-  Flex,
-  Reset,
-  ScrollArea,
-  Slot,
-  Spinner,
-  Table,
-  Tabs,
-} from '@radix-ui/themes'
-import { Allotment, AllotmentHandle, LayoutPriority } from 'allotment'
-import { CheckCircleIcon, CircleXIcon, TerminalIcon } from 'lucide-react'
-import {
-  forwardRef,
-  ReactNode,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from 'react'
+import { Box, Button, Flex, ScrollArea, Tabs } from '@radix-ui/themes'
+import { BugPlayIcon } from 'lucide-react'
+import { useCallback, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useMeasure } from 'react-use'
 
 import { EmptyMessage } from '@/components/EmptyMessage'
 import { FileNameHeader } from '@/components/FileNameHeader'
@@ -28,52 +11,15 @@ import { ReadOnlyEditor } from '@/components/Monaco/ReadOnlyEditor'
 import { RunInCloudDialog } from '@/components/RunInCloudDialog/RunInCloudDialog'
 import { ChecksSection } from '@/components/Validator/ChecksSection'
 import { LogsSection } from '@/components/Validator/LogsSection'
+import { RequestList } from '@/components/WebLogView'
 import { getRoutePath } from '@/routeMap'
 import { useToast } from '@/store/ui/useToast'
 import { StudioFile } from '@/types'
 import { getFileNameWithoutExtension } from '@/utils/file'
 
 import { useDebugSession, useScriptPath } from './Validator.hooks'
-import { ValidatorContent } from './ValidatorContent'
 import { ValidatorControls } from './ValidatorControls'
-import { ValidatorEmptyState } from './ValidatorEmptyState'
-
-function toDurationInSeconds(start: number, end: number) {
-  return ((end - start) / 1000).toFixed(1)
-}
-
-type ValidatorTabs = 'script' | 'requests' | 'checks' | 'browser'
-
-interface LogsTriggerProps {
-  count: number
-  onClick?: () => void
-}
-
-const LogsTrigger = forwardRef<HTMLDivElement, LogsTriggerProps>(
-  function LogsTrigger({ count, onClick }, ref) {
-    return (
-      <div ref={ref}>
-        <Reset>
-          <button
-            css={css`
-              font-size: var(--font-size-2);
-              font-weight: var(--font-weight-medium);
-              width: 100%;
-              display: flex;
-              align-items: center;
-              gap: var(--space-2);
-              padding: var(--space-2) var(--space-4);
-              background-color: var(--gray-2);
-            `}
-            onClick={onClick}
-          >
-            <TerminalIcon /> Logs ({count})
-          </button>
-        </Reset>
-      </div>
-    )
-  }
-)
+import { DebugSession } from './types'
 
 function TabContent({
   children,
@@ -88,11 +34,13 @@ function TabContent({
         align="stretch"
         direction="column"
       >
-        <ScrollArea>{children}</ScrollArea>
+        {children}
       </Flex>
     </Tabs.Content>
   )
 }
+
+type ValidatorTabs = 'script' | 'debugger'
 
 export function Validator() {
   const [currentTab, setCurrentTab] = useState<ValidatorTabs>('script')
@@ -119,12 +67,7 @@ export function Validator() {
       : undefined
 
   const handleTabChange = (tab: string) => {
-    if (
-      tab !== 'script' &&
-      tab !== 'requests' &&
-      tab !== 'checks' &&
-      tab !== 'browser'
-    ) {
+    if (tab !== 'script' && tab !== 'debugger') {
       return
     }
 
@@ -169,13 +112,13 @@ export function Validator() {
     navigate(getRoutePath('home'))
   }
 
-  async function handleRunScript() {
+  async function handleDebugScript() {
     if (!scriptPath) {
       return
     }
 
     setIsRunning(true)
-    setCurrentTab('requests')
+    setCurrentTab('debugger')
 
     await startDebugging()
   }
@@ -218,132 +161,48 @@ export function Validator() {
     <View
       title="Validator"
       subTitle={file ? <FileNameHeader file={file} /> : null}
+      loading={isLoading}
       actions={
         <ValidatorControls
           isRunning={isRunning}
           isExternal={isExternal}
           isScriptSelected={Boolean(scriptPath)}
           onDeleteScript={handleDeleteScript}
-          onRunScript={handleRunScript}
+          onRunScript={handleDebugScript}
           onRunInCloud={handleRunInCloud}
           onSelectScript={handleSelectExternalScript}
           onStopScript={handleStopScript}
         />
       }
-      loading={isLoading}
     >
-      <BottomDrawerAllotment
-        top={
-          <Tabs.Root asChild value={currentTab} onValueChange={handleTabChange}>
-            <Flex flexGrow="1" minHeight="100%" direction="column">
-              <Tabs.List>
-                <Tabs.Trigger value="script">Script</Tabs.Trigger>
-                {!pending && (
-                  <>
-                    <Tabs.Trigger value="requests" disabled={pending}>
-                      Requests ({session.requests.length})
-                    </Tabs.Trigger>
-                    <Tabs.Trigger value="browser" disabled={pending}>
-                      Browser ({session.browserActions.length})
-                    </Tabs.Trigger>
-                    <Tabs.Trigger value="checks" disabled={pending}>
-                      Checks ({session.checks.length})
-                    </Tabs.Trigger>
-                  </>
-                )}
-              </Tabs.List>
-              <TabContent value="script">
-                <div
-                  css={css`
-                    position: absolute;
-                    inset: 0;
-                  `}
-                >
-                  <ReadOnlyEditor value={script} language="typescript" />
-                </div>
-              </TabContent>
-              <TabContent value="requests">
-                <ValidatorContent
-                  script={script}
-                  session={session}
-                  isRunning={isRunning}
-                  noDataElement={
-                    <EmptyMessage
-                      message={
-                        <ValidatorEmptyState
-                          isRunning={isRunning}
-                          isScriptSelected={Boolean(scriptPath)}
-                          onRunScript={handleRunScript}
-                          onSelectScript={handleSelectExternalScript}
-                        />
-                      }
-                    />
-                  }
-                />
-              </TabContent>
-              <TabContent value="browser">
-                <Table.Root size="2" layout="fixed">
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.ColumnHeaderCell width="28px"></Table.ColumnHeaderCell>
-                      <Table.ColumnHeaderCell>Action</Table.ColumnHeaderCell>
-                      <Table.ColumnHeaderCell width="100px"></Table.ColumnHeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    {session.browserActions.map((event) => {
-                      return (
-                        <Table.Row key={event.eventId}>
-                          <Table.Cell>
-                            <Flex align="center">
-                              {event.type === 'begin' ? (
-                                <Spinner />
-                              ) : event.result.type === 'success' ? (
-                                <CheckCircleIcon
-                                  display="block"
-                                  color="var(--green-11)"
-                                />
-                              ) : (
-                                <CircleXIcon
-                                  display="block"
-                                  color="var(--red-11)"
-                                />
-                              )}
-                            </Flex>
-                          </Table.Cell>
-                          <Table.Cell>
-                            {event.action.type === 'goto' && (
-                              <>Go to {event.action.url}</>
-                            )}
-                            {event.action.type === 'click' && (
-                              <>Click {event.action.selector}</>
-                            )}
-                          </Table.Cell>
-                          <Table.Cell align="right" pr="2">
-                            {toDurationInSeconds(
-                              event.timestamp.started,
-                              event.type === 'end'
-                                ? event.timestamp.ended
-                                : Date.now()
-                            )}
-                            s
-                          </Table.Cell>
-                        </Table.Row>
-                      )
-                    })}
-                  </Table.Body>
-                </Table.Root>
-              </TabContent>
-              <TabContent value="checks">
-                <ChecksSection isRunning={isRunning} checks={session.checks} />
-              </TabContent>
+      <Tabs.Root asChild value={currentTab} onValueChange={handleTabChange}>
+        <Flex flexGrow="1" direction="column" align="stretch">
+          <Tabs.List>
+            <Tabs.Trigger value="script">Script</Tabs.Trigger>
+            <Tabs.Trigger value="debugger">Debugger</Tabs.Trigger>
+          </Tabs.List>
+          <TabContent value="script">
+            <Box position="absolute" inset="0">
+              <ReadOnlyEditor value={script} language="javascript" />
+            </Box>
+          </TabContent>
+          <TabContent value="debugger">
+            <Flex
+              css={css`
+                flex: 1 1 0;
+              `}
+              direction="column"
+              align="stretch"
+              justify="center"
+            >
+              {pending && (
+                <DebuggerEmptyState onDebugScript={handleDebugScript} />
+              )}
+              {!pending && <Debugger session={session} />}
             </Flex>
-          </Tabs.Root>
-        }
-        trigger={<LogsTrigger count={session.logs.length} />}
-        bottom={<LogsSection logs={session.logs} autoScroll={isRunning} />}
-      />
-
+          </TabContent>
+        </Flex>
+      </Tabs.Root>
       {scriptPath !== undefined && (
         <RunInCloudDialog
           open={showRunInCloudDialog}
@@ -358,97 +217,79 @@ export function Validator() {
   )
 }
 
-/**
- * This sets the ratio of the top and bottom to such an absurd degree that
- * the bottom pane is forced to take its minimum size.
- */
-const CLOSED_SIZE = [Number.MAX_SAFE_INTEGER, 1]
-
-interface BottomDrawerAllotmentProps {
-  defaultOpen?: boolean
-  defaultSizes?: [number, number]
-  trigger: ReactNode
-  bottom: ReactNode
-  top: ReactNode
+interface DebuggerEmptyStateProps {
+  onDebugScript: () => void
 }
 
-function BottomDrawerAllotment({
-  defaultOpen = false,
-  defaultSizes = [600, 200],
-  top,
-  trigger,
-  bottom,
-}: BottomDrawerAllotmentProps) {
-  const allotmentRef = useRef<AllotmentHandle>(null)
-  const initializedRef = useRef(false)
-
-  const [triggerRef, triggerDimensions] = useMeasure<HTMLElement>()
-
-  const [sizes, setSizes] = useState(defaultOpen ? defaultSizes : CLOSED_SIZE)
-  const [showDrawer, setShowDrawer] = useState(defaultOpen)
-
-  const initializeSizes = useCallback(
-    ([top = 0, bottom = 0]: number[]) => {
-      if (initializedRef.current) {
-        return
-      }
-
-      initializedRef.current = true
-
-      setSizes([top, bottom])
-
-      if (!showDrawer) {
-        allotmentRef.current?.resize(CLOSED_SIZE)
-      }
-    },
-    [showDrawer]
-  )
-
-  const handleDragEnd = useCallback(
-    ([top = 0, bottom = 0]: number[]) => {
-      const isMinimized = bottom <= triggerDimensions.height
-
-      setShowDrawer(!isMinimized)
-
-      if (!isMinimized) {
-        initializedRef.current = true
-
-        setSizes([top, bottom])
-      }
-    },
-    [triggerDimensions]
-  )
-
-  const handleToggle = useCallback(() => {
-    const shouldShow = !showDrawer
-
-    setShowDrawer(shouldShow)
-
-    allotmentRef.current?.resize(shouldShow ? sizes : CLOSED_SIZE)
-  }, [showDrawer, sizes])
-
+function DebuggerEmptyState({ onDebugScript }: DebuggerEmptyStateProps) {
   return (
-    <Allotment
-      ref={allotmentRef}
-      vertical={true}
-      defaultSizes={defaultSizes}
-      onDragEnd={handleDragEnd}
-      onChange={initializeSizes}
+    <EmptyMessage
+      message="Inspect what your script is doing while it runs."
+      action={
+        <Button onClick={onDebugScript}>
+          <BugPlayIcon /> Debug script
+        </Button>
+      }
+      justify="center"
+      maxHeight="800px"
+      pt="0"
+      illustration={undefined}
+    />
+  )
+}
+
+interface DebuggerProps {
+  session: DebugSession
+}
+
+function Debugger({ session }: DebuggerProps) {
+  return (
+    <Flex
+      css={css`
+        flex: 1 1 0;
+        background-color: var(--gray-2);
+      `}
+      p="2"
+      overflow="hidden"
+      direction="column"
     >
-      <Allotment.Pane priority={LayoutPriority.High}>{top}</Allotment.Pane>
-      <Allotment.Pane
-        priority={LayoutPriority.Low}
-        minSize={triggerDimensions.height}
-      >
-        <Slot
-          ref={triggerRef}
-          aria-expanded={showDrawer}
-          onClick={handleToggle}
+      <Tabs.Root asChild defaultValue="network">
+        <Flex
+          direction="column"
+          align="stretch"
+          css={css`
+            flex: 1 1 0;
+            background-color: var(--color-background);
+          `}
         >
-          {trigger}
-        </Slot>
-        {bottom}
-      </Allotment.Pane>
-    </Allotment>
+          <Tabs.List>
+            <Tabs.Trigger value="console">Console</Tabs.Trigger>
+            <Tabs.Trigger value="network">Network</Tabs.Trigger>
+            <Tabs.Trigger value="checks">Checks</Tabs.Trigger>
+          </Tabs.List>
+          <TabContent value="console">
+            <LogsSection autoScroll={false} logs={session.logs} />
+          </TabContent>
+          <Tabs.Content asChild value="network">
+            <div
+              css={css`
+                flex: 1 1 0;
+                overflow: hidden;
+              `}
+            >
+              <ScrollArea>
+                <RequestList
+                  requests={session.requests}
+                  onSelectRequest={() => {}}
+                />
+              </ScrollArea>
+            </div>
+          </Tabs.Content>
+          <TabContent value="checks">
+            <ChecksSection isRunning={false} checks={session.checks} />
+          </TabContent>
+        </Flex>
+      </Tabs.Root>
+    </Flex>
   )
 }
