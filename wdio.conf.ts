2f2e3f356782db2dadec0fd0946d525fd787a49f
Cristiano Ventura <mrk.cristiano@gmail.com>
2025-10-02 20:25:10 -0400

    change log level

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -69,7 +69,7 @@ export const config: WebdriverIO.Config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'silent',
+  logLevel: 'debug',
   //
   // Set specific log levels per logger
   // loggers:
