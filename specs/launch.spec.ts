328b6c950a3676f5cff017045c0e0daa4385cc58
Cristiano Ventura <mrk.cristiano@gmail.com>
2025-10-06 14:00:57 -0400

    Lint

--- specs/launch.spec.ts
+++ specs/launch.spec.ts
@@ -1,16 +1,21 @@
 import { browser, expect } from '@wdio/globals'
 
 describe('Launches the application', () => {
-  it('should main window', async () => {
-    browser.waitUntil(async () => {
-      return browser.electron.windowHandle !== undefined
-    }, { timeout: 10000 })
+  it('should show main window', async () => {
+    browser.waitUntil(
+      async () => {
+        return browser.electron.windowHandle !== undefined
+      },
+      { timeout: 10000 }
+    )
 
     browser.switchToWindow(browser.electron.windowHandle!)
-    
+
     expect(await browser.getTitle()).toBe('Grafana k6 Studio')
-    
+
     const welcomeMessage = browser.$('h1')
-    expect(await welcomeMessage.getText()).toBe('Discover what you can do with Grafana k6 Studio')
+    expect(await welcomeMessage.getText()).toBe(
+      'Discover what you can do with Grafana k6 Studio'
+    )
   })
 })
