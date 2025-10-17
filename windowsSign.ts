a6e45e4ec66488e8e1d36b4df1a46aebbdf2aa33
Llandy Riveron Del Risco <llandy3d@gmail.com>
2025-10-17 15:07:04 +0200

    chore: set up updated signtool

--- windowsSign.ts
+++ windowsSign.ts
@@ -4,5 +4,11 @@ export const windowsSign: WindowsSignOptions = {
   ...(process.env.SIGNTOOL_PATH
     ? { signToolPath: process.env.SIGNTOOL_PATH }
     : {}),
-  signWithParams: `code trusted-signing /v /debug -td sha256 --trusted-signing-account ${process.env.TRUSTED_SIGNING_ACCOUNT} --trusted-signing-certificate-profile ${process.env.TRUSTED_SIGNING_PROFILE} --trusted-signing-endpoint ${process.env.TRUSTED_SIGNING_ENDPOINT}`,
+  // signWithParams: `code trusted-signing /v /debug -td sha256 -fd sha256 --trusted-signing-account ${process.env.TRUSTED_SIGNING_ACCOUNT} --trusted-signing-certificate-profile ${process.env.TRUSTED_SIGNING_PROFILE} --trusted-signing-endpoint ${process.env.TRUSTED_SIGNING_ENDPOINT}`,
+  hookFunction: signingHook,
+}
+
+
+function signingHook(filePath: string) {
+  console.log('HELLO', filePath)
 }
