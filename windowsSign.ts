023d1cf0410afeaa07dd423d867c22c882a74cd8
Llandy Riveron Del Risco <llandy3d@gmail.com>
2025-10-17 14:13:45 +0200

    chore: set up updated signtool

--- windowsSign.ts
+++ windowsSign.ts
@@ -5,7 +5,7 @@ export const windowsSign: WindowsSignOptions = {
   ...(process.env.SIGNTOOL_PATH
     ? { signToolPath: process.env.SIGNTOOL_PATH }
     : {}),
-  signWithParams: `/v /debug --trusted-signing-account ${process.env.TRUSTED_SIGNING_ACCOUNT} --trusted-signing-certificate-profile ${process.env.TRUSTED_SIGNING_PROFILE} --trusted-signing-endpoint ${process.env.TRUSTED_SIGNING_ENDPOINT}`,
+  signWithParams: `code trusted-signing /v /debug --trusted-signing-account ${process.env.TRUSTED_SIGNING_ACCOUNT} --trusted-signing-certificate-profile ${process.env.TRUSTED_SIGNING_PROFILE} --trusted-signing-endpoint ${process.env.TRUSTED_SIGNING_ENDPOINT}`,
   timestampServer: 'http://timestamp.acs.microsoft.com',
   hashes: ['sha256' as HASHES],
 }
