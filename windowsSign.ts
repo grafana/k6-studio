cbb877c03bc12ee0e9ef34bc6d9d74db12a7b2a2
Llandy Riveron Del Risco <llandy3d@gmail.com>
2025-10-17 14:35:12 +0200

    chore: set up updated signtool

--- windowsSign.ts
+++ windowsSign.ts
@@ -1,11 +1,8 @@
 import type { WindowsSignOptions } from '@electron/packager'
-import type { HASHES } from '@electron/windows-sign/dist/esm/types'
 
 export const windowsSign: WindowsSignOptions = {
   ...(process.env.SIGNTOOL_PATH
     ? { signToolPath: process.env.SIGNTOOL_PATH }
     : {}),
-  signWithParams: `code trusted-signing /v /debug --trusted-signing-account ${process.env.TRUSTED_SIGNING_ACCOUNT} --trusted-signing-certificate-profile ${process.env.TRUSTED_SIGNING_PROFILE} --trusted-signing-endpoint ${process.env.TRUSTED_SIGNING_ENDPOINT}`,
-  timestampServer: 'http://timestamp.acs.microsoft.com',
-  hashes: ['sha256' as HASHES],
+  signWithParams: `code trusted-signing /v /debug -td sha256 --trusted-signing-account ${process.env.TRUSTED_SIGNING_ACCOUNT} --trusted-signing-certificate-profile ${process.env.TRUSTED_SIGNING_PROFILE} --trusted-signing-endpoint ${process.env.TRUSTED_SIGNING_ENDPOINT}`,
 }
