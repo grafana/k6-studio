d736494696bf6045b22b6a8ec4622a8cd3417292
Llandy Riveron Del Risco <llandy3d@gmail.com>
2025-10-29 16:42:27 +0100

    address pr feedback

--- windowsSignHook.ts
+++ windowsSignHook.ts
@@ -1,11 +1,26 @@
 import { spawn } from 'node:child_process'
 
-module.exports = function (filePath: string) {
+export default function (filePath: string) {
   console.log(`Path to file to sign: ${filePath}`)
 
   const signToolPath = process.env.SIGNTOOL_PATH
   if (!signToolPath) {
     throw new Error('SIGNTOOL_PATH environment variable is not set')
+  }
+
+  const trustedSigningAccount = process.env.TRUSTED_SIGNING_ACCOUNT
+  if (!trustedSigningAccount) {
+    throw new Error('TRUSTED_SIGNING_ACCOUNT environment variable is not set')
+  }
+
+  const trustedSigningProfile = process.env.TRUSTED_SIGNING_PROFILE
+  if (!trustedSigningProfile) {
+    throw new Error('TRUSTED_SIGNING_PROFILE environment variable is not set')
+  }
+
+  const trustedSigningEndpoint = process.env.TRUSTED_SIGNING_ENDPOINT
+  if (!trustedSigningEndpoint) {
+    throw new Error('TRUSTED_SIGNING_ENDPOINT environment variable is not set')
   }
 
   const args = [
@@ -17,11 +32,11 @@ module.exports = function (filePath: string) {
     '-fd',
     'sha256',
     '--trusted-signing-account',
-    process.env.TRUSTED_SIGNING_ACCOUNT!,
+    trustedSigningAccount,
     '--trusted-signing-certificate-profile',
-    process.env.TRUSTED_SIGNING_PROFILE!,
+    trustedSigningProfile,
     '--trusted-signing-endpoint',
-    process.env.TRUSTED_SIGNING_ENDPOINT!,
+    trustedSigningEndpoint,
   ]
 
   const signingProc = spawn(signToolPath, args, {
