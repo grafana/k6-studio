5f7bcc27f8c8cede0fd5890a772e7b31c96cdaa2
Edgar Fisher <fisher.edgar@gmail.com>
2025-12-09 15:44:02 +0200

    fix: update ref after change

--- src/hooks/useSettings.ts
+++ src/hooks/useSettings.ts
@@ -45,6 +45,7 @@ export function useSettingsChanged(onChanged: () => void) {
 
     if (prevDataRef.current !== data) {
       onChanged()
+      prevDataRef.current = data
     }
   }, [data, onChanged])
 }
