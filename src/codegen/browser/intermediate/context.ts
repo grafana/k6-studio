67df6b1992c8db4286fd878c12b8a5557cac3a7b
Johan Suleiko Allansson <allansson@gmail.com>
2026-02-16 17:38:05 +0100

    move CountedSet into separate file

--- src/codegen/browser/intermediate/context.ts
+++ src/codegen/browser/intermediate/context.ts
@@ -6,44 +6,7 @@ import { Graph } from '../graph'
 import * as model from '../types'
 
 import * as ir from './ast'
-
-class CountedSet<T> {
-  #map = new Map<T, number>()
-
-  constructor(initial?: [T, number][]) {
-    if (initial) {
-      for (const [value, count] of initial) {
-        this.#map.set(value, count)
-      }
-    }
-  }
-
-  add(value: T, count = 1) {
-    const currentCount = this.#map.get(value) ?? 0
-
-    this.#map.set(value, currentCount + count)
-  }
-
-  delete(value: T) {
-    const count = this.#map.get(value)
-
-    if (count === undefined) {
-      return false
-    }
-
-    if (count === 1) {
-      this.#map.delete(value)
-    } else {
-      this.#map.set(value, count - 1)
-    }
-
-    return true
-  }
-
-  get size() {
-    return this.#map.size
-  }
-}
+import { CountedSet } from './utils'
 
 type ScenarioGraph = Graph<model.TestNode, null>
 
