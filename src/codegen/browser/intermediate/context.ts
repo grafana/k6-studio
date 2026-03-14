5171f65008aee03a85db9162d1b5d65eccda08e5
Llandy Riveron Del Risco <llandy3d@gmail.com>
2026-03-14 03:39:21 +0100

    fix: browser export crashing

--- src/codegen/browser/intermediate/context.ts
+++ src/codegen/browser/intermediate/context.ts
@@ -46,6 +46,12 @@ function buildScenarioGraph(scenario: model.Scenario) {
         break
 
       case 'goto':
+        if (node.source !== 'implicit') {
+          graph.connect(node.nodeId, node.inputs.page.nodeId, null)
+        }
+        connectPrevious(graph, node)
+        break
+
       case 'reload':
         graph.connect(node.nodeId, node.inputs.page.nodeId, null)
         connectPrevious(graph, node)
@@ -53,6 +59,9 @@ function buildScenarioGraph(scenario: model.Scenario) {
 
       case 'click':
         graph.connect(node.nodeId, node.inputs.locator.nodeId, null)
+        if (node.triggersNavigation) {
+          graph.connect(node.nodeId, node.inputs.page.nodeId, null)
+        }
         connectPrevious(graph, node)
         break
 
