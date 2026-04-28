ef478309b9156359bf0485c5c518f48f2e15ef99
Johan Suleiko Allansson <allansson@gmail.com>
2026-04-28 14:37:52 +0200

    remove debug log

--- src/main/runner/shims/browser/index.ts
+++ src/main/runner/shims/browser/index.ts
@@ -42,9 +42,6 @@ interface OnEndContext {
 }
 
 if ('use' in expect && typeof expect.use === 'function') {
-  console.log(
-    'Registering k6-studio-tracking plugin for enhanced assertion tracking'
-  )
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   expect.use({
     name: 'k6-studio-tracking',
