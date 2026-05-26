22da0e4b45e2861f12ed3c3041492ae158ac4b4c
Johan Suleiko Allansson <allansson@gmail.com>
2026-05-26 17:57:19 +0200

    fix faulty logger import

--- src/utils/recentFiles.ts
+++ src/utils/recentFiles.ts
@@ -1,5 +1,5 @@
 import { app } from 'electron'
-import logger from 'electron-log'
+import logger from 'electron-log/main'
 import { z } from 'zod'
 
 import { readFileSync, writeFile } from '@/utils/fs'
